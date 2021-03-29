066af7da11fa2108d4f27ebdcaa0681a48889959
Illia Polosukhin <ilblackdragon@gmail.com>
2021-03-29 09:54:42 -0700

    Use default contract name for mainnet

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'ref-ui-new'
+const CONTRACT_NAME = process.env.CONTRACT_NAME || 'ref-finance.testnet'
 
 function getConfig(env) {
   switch (env) {
@@ -8,7 +8,7 @@ function getConfig(env) {
     return {
       networkId: 'mainnet',
       nodeUrl: 'https://rpc.mainnet.near.org',
-      contractName: CONTRACT_NAME,
+      contractName: 'ref-finance.near',
       walletUrl: 'https://wallet.near.org',
       helperUrl: 'https://helper.mainnet.near.org',
       explorerUrl: 'https://explorer.mainnet.near.org',
