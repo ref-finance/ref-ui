d29ac6b8a8ecc35afa2649fc1f05fade0a11ee54
Josh <thisisjoshford@gmail.com>
2021-04-02 13:03:26 -0700

    add header for portfolio

--- src/pages/PortfolioPage.tsx
+++ src/pages/PortfolioPage.tsx
@@ -12,6 +12,7 @@ export default function PortfolioPage() {
 
   return (
     <section className="bg-secondary shadow-2xl rounded p-8 sm:w-full md:w-1/4 lg:w-1/2 m-auto place-self-center">
+      <h1 className="text-center text-xl font-bold">Account Balances</h1>
       <TokenList
         tokens={registeredTokens}
         render={(token) => (
