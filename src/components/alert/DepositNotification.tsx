af82a7df8daffcb7a68bf200f5028181b8842114
Ryan Mehta <ryan.mehta@gmail.com>
2021-04-02 16:01:07 -0700

    buttons

--- src/components/alert/DepositNotification.tsx
+++ src/components/alert/DepositNotification.tsx
@@ -14,32 +14,29 @@ export default function DepositNotification({ open }: { open: boolean }) {
             your account which requires a small amount of initial storage (.01 Ⓝ
             )
           </p>
-          <button
-            className="items-center  my-4 h-10 w-40 rounded-md bg-buttonBg border-2 text-buttonText hover:bg-buttonText hover:text-buttonBg hover:border-buttonBg hover:border-2 shadow-lg hover:bg-disabled rounded-lg transition-colors focus:outline-none"
-            onClick={() =>
-              depositStorage({
-                accountId: wallet.getAccountId(),
-              })
-            }
-          >
-            {' '}
-            PROCEED
-          </button>
-          <button
-            className="bg-red-600 hover:bg-white flex-row-centered hover:text-red-600 hover:border-2 hover:border-red-600 text-white flex flex-col my-4 h-10 w-40 border-2  shadow-lg hover:bg-disabled rounded-lg transition-colors focus:outline-none"
-            onClick={() => close()}
-          >
-            {' '}
-            SIGN OUT
-          </button>
-          <button
-            onClick={() => {
-              wallet.signOut();
-              window.location.replace('/');
-            }}
-          >
-            No
-          </button>
+          <section className="flex justify-center">
+            <button
+              className="items-center  my-4 h-10 w-40 rounded-md bg-buttonBg border-2 text-buttonText hover:bg-buttonText hover:text-buttonBg hover:border-buttonBg hover:border-2 shadow-lg hover:bg-disabled rounded-lg transition-colors focus:outline-none"
+              onClick={() =>
+                depositStorage({
+                  accountId: wallet.getAccountId(),
+                })
+              }
+            >
+              {' '}
+              PROCEED
+            </button>
+            <button
+              className="bg-red-600 hover:bg-white flex-row-centered hover:text-red-600 hover:border-2 hover:border-red-600 text-white flex flex-col my-4 h-10 w-40 border-2  shadow-lg hover:bg-disabled rounded-lg transition-colors focus:outline-none"
+              onClick={() => {
+                wallet.signOut();
+                window.location.replace('/');
+              }}
+            >
+              {' '}
+              SIGN OUT
+            </button>
+          </section>
         </>
       )}
     </MicroModal>
