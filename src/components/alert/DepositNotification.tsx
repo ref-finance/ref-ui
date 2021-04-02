916e329a390a709b293c4c2e68878c75cacfe9ad
Josh <thisisjoshford@gmail.com>
2021-04-02 15:57:11 -0700

    initial storage copy + style

--- src/components/alert/DepositNotification.tsx
+++ src/components/alert/DepositNotification.tsx
@@ -9,17 +9,22 @@ export default function DepositNotification({ open }: { open: boolean }) {
     <MicroModal openInitially={open}>
       {(close) => (
         <>
-          <p>Blah Blah</p>
+          <h1 className="text-center text-2xl">First time here?</h1>
+          <p className="my-4">Before you can use Re-Fi exchange you will first need to register your account which requires a small amount of initial storage (.01 Ⓝ )</p>
           <button
+            className="items-center  my-4 h-10 w-40 rounded-md bg-buttonBg border-2 text-buttonText hover:bg-buttonText hover:text-buttonBg hover:border-buttonBg hover:border-2 shadow-lg hover:bg-disabled rounded-lg transition-colors focus:outline-none"
             onClick={() =>
               depositStorage({
                 accountId: wallet.getAccountId(),
               })
             }
-          >
-            Yes
+          > PROCEED
           </button>
-          <button onClick={() => close()}>No</button>
+          <button
+            className="bg-red-600 hover:bg-white flex-row-centered hover:text-red-600 hover:border-2 hover:border-red-600 text-white flex flex-col my-4 h-10 w-40 border-2  shadow-lg hover:bg-disabled rounded-lg transition-colors focus:outline-none" 
+            onClick={() => close()}
+          > SIGN OUT
+          </button>
         </>
       )}
     </MicroModal>
