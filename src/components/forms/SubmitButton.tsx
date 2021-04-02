ad0a404614547412d35db8d78618ecde79f04040
Josh <thisisjoshford@gmail.com>
2021-04-02 12:06:16 -0700

    remove focus line

--- src/components/forms/SubmitButton.tsx
+++ src/components/forms/SubmitButton.tsx
@@ -16,7 +16,7 @@ function SubmitButton({ text, disabled, onClick }: SubmitButtonProps) {
           type={onClick ? 'button' : 'submit'}
           disabled={disabled}
           onClick={onClick}
-          className="flex flex-row justify-center py-4 mt-5 mb-5 items-center rounded-md w-full bg-buttonBg border-2 text-buttonText hover:bg-buttonText hover:text-buttonBg hover:border-buttonBg hover:border-2 disabled:opacity-50 transition-colors shadow-lg transition-colors"
+          className="flex flex-row justify-center py-4 mt-5 mb-5 items-center rounded-md w-full bg-buttonBg border-2 text-buttonText hover:bg-buttonText hover:text-buttonBg hover:border-buttonBg hover:border-2 disabled:opacity-50 transition-colors shadow-lg transition-colors focus:outline-none"
         >
           <h1 className="text-xl font-inter font-medium ">{text}</h1>
         </button>
@@ -24,7 +24,7 @@ function SubmitButton({ text, disabled, onClick }: SubmitButtonProps) {
         <button
           onClick={signIn}
           type="button"
-          className="bg-blue-500 hover:bg-blue-600 text-white py-4 mt-5 mb-5 items-center rounded-md w-full border border-black shadow-lg hover:bg-disabled rounded-lg transition-colors"
+          className="bg-blue-500 hover:bg-blue-600 text-white py-4 mt-5 mb-5 items-center rounded-md w-full border border-black shadow-lg hover:bg-disabled rounded-lg transition-colors focus:outline-none"
         >
           {' '}
           CONNECT WALLET
