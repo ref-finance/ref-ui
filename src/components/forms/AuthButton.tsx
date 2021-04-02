a1b5e78c26604e4428bf85225eb0faa01483de9b
Josh <thisisjoshford@gmail.com>
2021-04-02 12:03:48 -0700

    update signout button css

--- src/components/forms/AuthButton.tsx
+++ src/components/forms/AuthButton.tsx
@@ -18,7 +18,7 @@ function AuthButton() {
           <button
             onClick={signOut}
             type="button"
-            className="bg-red-600 hover:bg-red-500 text-white flex flex-col my-4 py-1 h-15 w-40 border border-black shadow-lg hover:bg-disabled rounded-lg transition-colors"
+            className="bg-red-600 hover:bg-white hover:text-red-600 hover:border-2 hover:border-red-600 text-white flex flex-col my-4 py-1 h-15 w-40 border-2  shadow-lg hover:bg-disabled rounded-lg transition-colors"
           >
             {' '}
             <p className="w-full">SIGN OUT</p>
