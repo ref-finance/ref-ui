393b0e8f648a1f5b7ebb1102dc833c41ba3d87e8
Ryan Mehta <ryan.mehta@gmail.com>
2021-04-02 14:30:48 -0700

    update tabs

--- src/components/forms/TabFormWrap.tsx
+++ src/components/forms/TabFormWrap.tsx
@@ -8,7 +8,7 @@ export default function TabFormWrap({
 
   return (
     <section className="flex justify-center">
-      <section className="m-8">
+      <section className="m-8 bg-secondary rounded">
         <nav className="flex flex-col sm:flex-row">
           {titles.map((title, i) => (
             <button
