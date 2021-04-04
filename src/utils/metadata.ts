a0da469b679e70fc17d430458d66800ece08f2b2
Josh <thisisjoshford@gmail.com>
2021-04-04 16:16:18 -0700

    add icon metadata

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,4 +1,23 @@
 export default {
   'wrap.near': 'https://fluxprotocol.eth.link/static/media/wrapped-near.8b3a5e4b.svg',
-  
+  "6b175474e89094c44da98b954eedeac495271d0f.factory.bridge.near": 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
+  "berryclub.ek.near" : "🍌",
+  "dac17f958d2ee523a2206206994597c13d831ec7.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/825.png",
+  "1f9840a85d5af5bf1d1762f925bdaddc4201f984.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/7083.png",
+  "514910771af9ca656af840dff83e8264ecf986ca.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/1975.png",
+  "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
+  "2260fac5e5542a773aa44fbcfedf7c193bc2c599.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/3717.png",
+  "7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/7278.png",
+  "a0b73e1ff0b80914ab6fe0444e65848c4c34450b.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/3635.png",
+  "50d1c9771902476076ecfc8b2a83ad6b9355a4c9.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/4195.png",
+  "4fabb145d64652a948d72533023f6e7a623c7c53.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/4687.png",
+  "6f259637dcd74c767781e37bc6133cd6a68aa161.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/2502.png",
+  "6b3595068778dd592e39a122f4f5a5cf09c90fe2.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/6758.png",
+  "c011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/2586.png",
+  "c944e90c64b2c07662a292be6244bdf05cda44a7.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/6719.png",
+  "9f8f72aa9304c8b593d555f12ef6589cc3a579a2.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/1518.png",
+  "0bc529c00c6401aef6d220be8c6ea1667f6ad93e.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/5864.png",
+  "c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/2396.png",
+  "0316eb71485b0ab14103307bf65a021042c6d380.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/6941.png",
+  "111111111117dc0aa78b770fa6a738034120c302.factory.bridge.near" : "https://s2.coinmarketcap.com/static/img/coins/64x64/8104.png"
 }
