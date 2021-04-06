841b9f85656cb64c1c2ae8109f97d768f68cdbf6
Josh <thisisjoshford@gmail.com>
2021-04-05 22:12:05 -0700

    add conditional withdraw info copy

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,5 +1,7 @@
 export default {
   slippageCopy: 'Slippage is the difference between the estimated <br> and actual price you receive. The percentage you <br> select is the acceptable difference between the <br> current exchange rate and the exchange rate at <br> the time of your transaction\'s execution.',
   deposit: 'This deposits your selected tokens into the <br> exchange for swapping or adding to a liquidity pool.',
-  nearDeposit: 'This first wraps then deposits your Ⓝ into the <br> exchange for swapping or adding to a liquidity pool.'
+  nearDeposit: 'This first wraps then deposits your Ⓝ into the <br> exchange for swapping or adding to a liquidity pool.',
+  nearWithdraw: 'This will first unwrap your Ⓝ then withdraw it from <br> the exchange and move the tokens to your wallet.',
+  withdraw: 'This will withdraw your selected tokens from <br> the exchange and deposit them into your wallet.'
 }
