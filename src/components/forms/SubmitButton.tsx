0d50ea032741065cb0fa9f39894af79f71df3607
Josh <thisisjoshford@gmail.com>
2021-04-06 00:23:43 -0700

    conditionally render info icon

--- src/components/forms/SubmitButton.tsx
+++ src/components/forms/SubmitButton.tsx
@@ -7,7 +7,7 @@ interface SubmitButtonProps {
   text: string;
   disabled?: boolean;
   onClick?: (event: React.MouseEvent) => void;
-  info: string;
+  info?: string;
 }
 
 function SubmitButton({ text, disabled, onClick, info }: SubmitButtonProps) {
@@ -21,14 +21,15 @@ function SubmitButton({ text, disabled, onClick, info }: SubmitButtonProps) {
           className="flex flex-row justify-center py-4 mt-5 mb-5 items-center rounded-md w-full bg-buttonBg border-2 text-buttonText hover:bg-buttonText hover:text-buttonBg hover:border-buttonBg hover:border-2 disabled:opacity-50 transition-colors shadow-lg transition-colors focus:outline-none disabled:cursor-not-allowed"
         >
           <h1 className="text-xl font-inter font-medium ">{text}</h1>
-          <FaInfoCircle
-            data-type="dark"
-            data-place="bottom"
-            data-multiline={true}
-            data-tip={info}
-            className="text-large ml-2"
-          />
-          <ReactTooltip />
+          {info ? (
+            <FaInfoCircle
+              data-type="dark"
+              data-place="bottom"
+              data-multiline={true}
+              data-tip={info}
+              className="text-large ml-2"
+            />
+          ) : null}
         </button>
       ) : (
         <button
