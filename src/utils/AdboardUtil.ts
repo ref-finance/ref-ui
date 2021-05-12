3b36d8bcd313f4c1511a54e2800a820ea798a495
Daniel <manakuchen@googlemail.com>
2021-05-12 17:50:18 +0200

    colorpalette fix

--- src/utils/AdboardUtil.ts
+++ src/utils/AdboardUtil.ts
@@ -63,7 +63,7 @@ export class AdboardUtil {
             this.colorPalette = this.colorPalette.concat(['#FF5722', '#FFCCBC', '#FFAB91', '#FF8A65', '#FF7043', '#F4511E', '#E64A19', '#D84315', '#BF360C', '#FF9E80', '#FF6E40', '#FF3D00', '#DD2C00']);
             this.colorPalette = this.colorPalette.concat(['#795548', '#D7CCC8', '#BCAAA4', '#A1887F', '#8D6E63', '#6D4C41', '#5D4037', '#4E342E', '#3E2723', '#9E9E9E', '#FAFAFA', '#EEEEEE', '#E0E0E0']);
             this.colorPalette = this.colorPalette.concat(['#BDBDBD', '#616161', '#424242', '#212121', '#607D8B', '#B0BEC5', '#90A4AE', '#78909C', '#607D8C', '#546E7A', '#455A64', '#37474F', '#263238']);
-            this.colorPalette = this.colorPalette.concat(['#000000', '#FFFFFF', '01C08B']);
+            this.colorPalette = this.colorPalette.concat(['#000000', '#FFFFFF', '#01C08B']);
 
 
 
