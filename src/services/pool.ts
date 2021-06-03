ea12fc731c3fa73f130dd990f38724ba4832c260
Joe Chu <aidai524@gmail.com>
2021-06-03 18:27:37 +0800

    fix add and remove liquidity for pool id 0 bugs (#93)

--- src/services/pool.ts
+++ src/services/pool.ts
@@ -39,7 +39,7 @@ export interface Pool {
 }
 
 const parsePool = (pool: PoolRPCView, id?: number): Pool => ({
-  id: id > 0 ? id : pool.id,
+  id: id >= 0 ? id : pool.id,
   tokenIds: pool.token_account_ids,
   supplies: pool.amounts.reduce(
     (acc: { [tokenId: string]: string }, amount: string, i: number) => {
