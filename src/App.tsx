72a15d44243389cbc8c4170535737c7ce14dc893
Joe Chu <aidai524@gmail.com>
2021-06-08 15:25:00 +0800

    Update swap page (#94)
    * fix add and remove liquidity for pool id 0 bugs
    
    * update background style
    
    * update swap page location hash format

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { AddPoolPage } from './pages/pools/AddPoolPage';
 import { AddTokenPage } from './pages/pools/AddTokenPage';
 import AdboardPage from './pages/Adboard/AdboardPage';
 import NavigationBar from './components/layout/NavigationBar';
-import { BgShapeLeftBottom } from './components/icon';
+import { BgShapeLeftBottom, BgShapeTopRight } from './components/icon';
 import Modal from 'react-modal';
 
 import './global.css';
@@ -45,6 +45,7 @@ function App() {
       <ToastContainer />
       <div className="relative h-screen">
         <BgShapeLeftBottom />
+        <BgShapeTopRight />
         <NavigationBar />
         <div className="justify-center h-4/5 mt-24">
           <Switch>
