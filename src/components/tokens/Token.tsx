6a66bc543238261bfbdfde58ef5b24cd10f852bb
xmly <xmly@xmlydeMacBook-Pro.local>
2021-10-22 23:53:42 +0800

    hotfix: modify the font size in small screen

--- src/components/tokens/Token.tsx
+++ src/components/tokens/Token.tsx
@@ -19,7 +19,7 @@ export default function Token({ token, onClick, sortBy }: TokenProps) {
       className="hover:bg-black hover:bg-opacity-10"
       onClick={() => onClick && onClick(token)}
     >
-      <td className="text-sm pl-6 py-5">
+      <td className="xs:text-xs text-sm pl-6 py-5">
         {icon ? (
           <img
             className="h-6 w-6 mr-3 inline-block"
@@ -31,13 +31,25 @@ export default function Token({ token, onClick, sortBy }: TokenProps) {
         )}
         <span className="inline-block text-white">{toRealSymbol(symbol)}</span>
       </td>
-      <td className={`py-4 ${sortBy === 'near' ? 'text-white' : ''}`}>
+      <td
+        className={`py-4 xs:text-xs text-sm ${
+          sortBy === 'near' ? 'text-white' : ''
+        }`}
+      >
         {toInternationalCurrencySystem(String(near)).replace(/[\,]+/g, '')}
       </td>
-      <td className={`py-4 ${sortBy === 'ref' ? 'text-white' : ''}`}>
+      <td
+        className={`py-4 xs:text-xs text-sm ${
+          sortBy === 'ref' ? 'text-white' : ''
+        }`}
+      >
         {toInternationalCurrencySystem(String(ref))}
       </td>
-      <td className={`pr-6 py-4 ${sortBy === 'total' ? 'text-white' : ''}`}>
+      <td
+        className={`pr-6 xs:text-xs text-sm py-4 ${
+          sortBy === 'total' ? 'text-white' : ''
+        }`}
+      >
         {toInternationalCurrencySystem(String(total))}
       </td>
     </tr>
