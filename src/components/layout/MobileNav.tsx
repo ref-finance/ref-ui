7ebd1e0fcb47cf7c0c4addd8bda0f36d79f95f24
ZhenhuaChen <1chenzhenhua1@gmail.com>
2021-11-06 17:45:37 +0800

    feat: refine the mobile navigation code

--- src/components/layout/MobileNav.tsx
+++ src/components/layout/MobileNav.tsx
@@ -1,4 +1,10 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, {
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { matchPath } from 'react-router';
 import { Context } from '~components/wrapper';
 import { Near, NavLogo, NavLogoLarge } from '~components/icon';
@@ -15,6 +21,7 @@ import { RiLogoutCircleRLine } from 'react-icons/ri';
 import { useRefPrice } from '~state/account';
 import { toPrecision } from '~utils/numbers';
 import { RefAnalytics } from '~components/icon/RefAnalytics';
+import { moreLinks } from '~utils/menu';
 
 export function MobileAnchor({
   to,
@@ -40,7 +47,7 @@ export function MobileAnchor({
     <div>
       <Link onClick={onClick} to={to}>
         <div
-          className={`p-4 link font-bold p-2 ${className} ${
+          className={`p-4 text-lg link font-bold p-2 ${className} ${
             isSelected ? 'text-white bg-navHighLightBg' : 'text-primaryText'
           }`}
         >
@@ -51,134 +58,15 @@ export function MobileAnchor({
   );
 }
 
-export function MobilePoolsMenu({
-  links,
-  onClick,
-}: {
-  links: Array<{ label: JSX.Element; path: string }>;
-  onClick: () => void;
-}) {
-  const location = useLocation();
-  const isSelected = location.pathname.startsWith('/pools');
-  const [show, setShow] = useState(isSelected);
-  const history = useHistory();
-
-  return (
-    <div className="relative z-20">
-      <div
-        className="flex p-4 items-center justify-between"
-        onClick={() => {
-          setShow(!show);
-        }}
-      >
-        <div
-          className={`text-white link font-bold ${
-            isSelected ? 'text-white' : 'text-primaryText'
-          }`}
-        >
-          <FormattedMessage id="pools" defaultMessage="Pools" />
-        </div>
-        <FiChevronUp
-          className={`${show ? 'inline-block' : 'hidden'} text-xl`}
-        />
-        <FiChevronDown
-          className={`${!show ? 'inline-block' : 'hidden'} text-xl`}
-        />
-      </div>
-      <div className={`${show ? 'block' : 'hidden'}`}>
-        {links.map((link) => {
-          const isSelected = link.path === location.pathname;
-
-          return (
-            <div
-              key={link.path}
-              className={`whitespace-nowrap text-left font-bold text-white p-4 ${
-                isSelected
-                  ? 'text-white bg-navHighLightBg'
-                  : 'text-primaryText bg-mobile-nav'
-              }`}
-              onClick={() => {
-                onClick();
-                history.push(link.path);
-              }}
-            >
-              {link.label}
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
-
-export function MobileMoreMenu({
-  links,
-  onClick,
-}: {
-  links: Array<{ label: string; url: string; isExternal: boolean }>;
-  onClick: () => void;
-}) {
-  const location = useLocation();
-  const isSelected = location.pathname.startsWith('/airdrop');
-  const [show, setShow] = useState(isSelected);
-
-  return (
-    <div className="relative z-20">
-      <div
-        className="flex p-4 items-center justify-between"
-        onClick={() => setShow(!show)}
-      >
-        <div
-          className={`text-white link font-bold ${
-            isSelected ? 'text-white' : 'text-primaryText'
-          }`}
-        >
-          <FormattedMessage id="more" defaultMessage="More" />
-        </div>
-        <FiChevronUp
-          className={`${show ? 'inline-block' : 'hidden'} text-xl`}
-        />
-        <FiChevronDown
-          className={`${!show ? 'inline-block' : 'hidden'} text-xl`}
-        />
-      </div>
-      <div className={`${show ? 'block' : 'hidden'}`}>
-        {links.map((link) => {
-          const isSelected = link.url === location.pathname;
-          return (
-            <div
-              key={link.url}
-              className={`whitespace-nowrap text-left font-bold text-white p-4 ${
-                isSelected
-                  ? 'text-white bg-navHighLightBg'
-                  : 'text-primaryText bg-mobile-nav'
-              }`}
-              onClick={() => {
-                onClick();
-                window.open(link.url, link.isExternal ? '' : '_self');
-              }}
-            >
-              {link.label}
-              {link.isExternal ? (
-                <FaExternalLinkAlt className="float-right mt-1 ml-2 text-xs opacity-60" />
-              ) : null}
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
-
 export function MobileSwitchLanguage() {
   const context = useContext(Context);
   const currentLocal = localStorage.getItem('local');
   const [show, setShow] = useState(false);
 
   return (
-    <div className="relative z-20">
+    <div className="relative z-20 bg-cardBg">
       <div
-        className="flex p-4 items-center justify-between"
+        className="flex p-4 items-center text-lg justify-between"
         onClick={() => setShow(!show)}
       >
         <div className={'font-bold text-primaryText'}>
@@ -191,22 +79,18 @@ export function MobileSwitchLanguage() {
           className={`${!show ? 'inline-block' : 'hidden'} text-xl`}
         />
       </div>
-      <div className={`${show ? 'block' : 'hidden'}`}>
+      <div className={`ml-12 ${show ? 'block' : 'hidden'}`}>
         <div
-          className={`whitespace-nowrap text-left font-bold text-white p-4 ${
-            currentLocal === 'en'
-              ? 'text-white bg-navHighLightBg'
-              : 'text-primaryText bg-mobile-nav'
+          className={`whitespace-nowrap bg-cardBg text-left font-bold text-white p-4 ${
+            currentLocal === 'en' ? 'text-white' : 'text-primaryText'
           }`}
           onClick={() => context.selectLanguage('en')}
         >
           English
         </div>
         <div
-          className={`whitespace-nowrap text-left font-bold text-white p-4 ${
-            currentLocal === 'zh-CN'
-              ? 'text-white bg-navHighLightBg'
-              : 'text-primaryText bg-mobile-nav'
+          className={`whitespace-nowrap text-left bg-cardBg font-bold text-white p-4 ${
+            currentLocal === 'zh-CN' ? 'text-white' : 'text-primaryText '
           }`}
           onClick={() => context.selectLanguage('zh-CN')}
         >
@@ -222,7 +106,7 @@ export function Logout() {
     wallet.isSignedIn() && (
       <div
         className={
-          'whitespace-nowrap flex text-left font-bold p-4 text-primaryText bg-mobile-nav'
+          'whitespace-nowrap flex text-left font-bold p-4 text-primaryText bg-cardBg'
         }
         onClick={() => {
           wallet.signOut();
@@ -243,6 +127,9 @@ export function MobileNavBar() {
   const iconRef = useRef<HTMLSpanElement | null>(null);
   const popupRef = useRef<HTMLDivElement | null>(null);
   const accountId = wallet.getAccountId();
+  const [openMenu, setOpenMenu] = useState('');
+  const [closeMenu, setCloseMenu] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
     document.addEventListener('click', handleClick, false);
@@ -251,59 +138,6 @@ export function MobileNavBar() {
       document.addEventListener('click', handleClick, false);
     };
   }, []);
-
-  const links = [
-    {
-      label: <FormattedMessage id="view_pools" defaultMessage="View Pools" />,
-      path: '/pools',
-    },
-    {
-      label: (
-        <FormattedMessage
-          id="Create_New_Pool"
-          defaultMessage="Create New Pool"
-        />
-      ),
-      path: '/pools/add',
-    },
-  ];
-
-  const moreLinks = [
-    {
-      label: <FormattedMessage id="airdrop" defaultMessage="Airdrop" />,
-      url: '/airdrop',
-      isExternal: false,
-    },
-    {
-      label: (
-        <FormattedMessage id="rainbow_bridge" defaultMessage="RainBow Bridge" />
-      ),
-      url: 'https://ethereum.bridgetonear.org/',
-      isExternal: true,
-    },
-    {
-      label: intl.formatMessage({ id: 'docs' }),
-      url: 'https://guide.ref.finance',
-      isExternal: true,
-    },
-    { label: 'Forum', url: 'https://gov.ref.finance', isExternal: true },
-    {
-      label: 'Discord',
-      url: 'https://discord.gg/SJBGcfMxJz',
-      isExternal: true,
-    },
-    { label: 'Telegram', url: 'https://t.me/ref_finance', isExternal: true },
-    {
-      label: 'Twitter',
-      url: 'https://twitter.com/finance_ref',
-      isExternal: true,
-    },
-    {
-      label: 'Medium',
-      url: 'https://ref-finance.medium.com/',
-      isExternal: true,
-    },
-  ];
 
   const handleClick = (e: any) => {
     if (
@@ -315,17 +149,37 @@ export function MobileNavBar() {
     setShow(false);
   };
 
+  // if (wallet.isSignedIn()) {
+  //   links.push({
+  //     label: (
+  //       <FormattedMessage id="Your_Liquidity" defaultMessage="Your Liquidity" />
+  //     ),
+  //     path: '/pools/yours',
+  //   });
+  // }
   if (wallet.isSignedIn()) {
-    links.push({
-      label: (
-        <FormattedMessage id="Your_Liquidity" defaultMessage="Your Liquidity" />
-      ),
-      path: '/pools/yours',
-    });
+    moreLinks[2].children[2] = {
+      id: 'Your_Liquidity',
+      label: 'Your Liquidity',
+      url: '/pools/yours',
+      pattern: '/pools/yours',
+      isExternal: false,
+    };
   }
 
   function close() {
     setShow(false);
+  }
+  function handleMenuClick(url: string, label: string, isExternal: boolean) {
+    if (url) {
+      isExternal ? window.open(url) : history.push(url);
+      close();
+    } else if (openMenu === label) {
+      setCloseMenu(!closeMenu);
+    } else {
+      setOpenMenu(label);
+      setCloseMenu(true);
+    }
   }
 
   return (
@@ -378,7 +232,7 @@ export function MobileNavBar() {
       >
         <div
           ref={popupRef}
-          className="block h-full w-4/6 float-right pt-6 bg-mobile-nav shadow-4xl"
+          className="block h-full w-4/6 float-right pt-6 bg-cardBg shadow-4xl"
         >
           <div className="flex justify-between items-center">
             <div
@@ -426,40 +280,112 @@ export function MobileNavBar() {
                 onClick={close}
               />
             )}
-            <MobileAnchor
-              to="/deposit"
-              pattern="/deposit/:id?"
-              name="Deposit"
-              onClick={close}
-            />
-            <MobileAnchor to="/" pattern="/" name="Swap" onClick={close} />
-            <MobilePoolsMenu links={links} onClick={close} />
-            <MobileAnchor
-              to="/farms"
-              pattern="/farms"
-              name="Farms"
-              onClick={close}
-            />
-            <div>
-              <Link
-                to={{
-                  pathname: 'https://mzko2gfnij6.typeform.com/to/EPmUetxU',
-                }}
-                target="_blank"
-              >
-                <div className="p-4 link font-bold p-2 text-primaryText">
-                  Quiz
-                  <FaExternalLinkAlt className="float-right mt-1 ml-2 text-xs opacity-60" />
-                </div>
-              </Link>
-            </div>
+            {moreLinks.map(
+              ({ id, label, subRoute, pattern, url, isExternal, children }) => {
+                const location = useLocation();
+                const isSelected = subRoute
+                  ? subRoute.includes(location.pathname)
+                  : matchPath(location.pathname, {
+                      path: pattern,
+                      exact: true,
+                      strict: false,
+                    });
+                return (
+                  <div key={id}>
+                    <div
+                      className={`flex p-4 items-center text-lg justify-between ${
+                        isSelected
+                          ? !children
+                            ? 'bg-navHighLightBg text-white'
+                            : 'text-white'
+                          : 'text-primaryText'
+                      }`}
+                      onClick={() => handleMenuClick(url, label, isExternal)}
+                    >
+                      <div className={`link `}>
+                        <FormattedMessage id={id} defaultMessage={label} />
+                      </div>
+                      {children && (
+                        <span>
+                          <FiChevronUp
+                            className={`${
+                              openMenu === label && closeMenu
+                                ? 'inline-block'
+                                : 'hidden'
+                            } text-xl`}
+                          />
+                          <FiChevronDown
+                            className={`${
+                              openMenu !== label || !closeMenu
+                                ? 'inline-block'
+                                : 'hidden'
+                            } text-xl`}
+                          />
+                        </span>
+                      )}
+                    </div>
+                    {children && (
+                      <div
+                        className={`${
+                          openMenu === label && closeMenu ? 'block' : 'hidden'
+                        }`}
+                      >
+                        {children?.map((link) => {
+                          const isSubMenuSelected = matchPath(
+                            location.pathname,
+                            {
+                              path: link.pattern,
+                              exact: true,
+                              strict: false,
+                            }
+                          );
+                          return (
+                            <div
+                              key={link.id}
+                              className={`whitespace-nowrap text-left pl-12 p-4 flex justify-start ${
+                                !link.isExternal && isSubMenuSelected
+                                  ? 'text-white bg-navHighLightBg'
+                                  : 'text-primaryText'
+                              }`}
+                              onClick={() => {
+                                link.url && link.isExternal
+                                  ? window.open(link.url)
+                                  : history.push(link.url);
+                                close();
+                              }}
+                            >
+                              {link.logo && (
+                                <span className="text-2xl mr-5">
+                                  {link.logo}
+                                </span>
+                              )}
+                              <FormattedMessage
+                                id={link.id}
+                                defaultMessage={link.label}
+                              />
+                              {link.tip && (
+                                <span className="ml-2 bg-gradientFrom px-2 flex justify-center items-center text-white text-xs rounded-full">
+                                  {link.tip}
+                                </span>
+                              )}
 
-            <MobileMoreMenu links={moreLinks} onClick={close} />
+                              {link.url && link.isExternal && (
+                                <FaExternalLinkAlt className="float-right mt-1 ml-2 text-xs opacity-60" />
+                              )}
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+                );
+              }
+            )}
             <MobileSwitchLanguage />
           </div>
           <Logout />
           <div
-            className=" p-4 "
+            className=" p-4 bg-cardBg"
             onClick={() => window.open('https://sodaki.com/')}
           >
             <RefAnalytics />
