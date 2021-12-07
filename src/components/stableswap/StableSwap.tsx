dcd206c8077c1c786f4864a1fdabc9442127fc0b
maxwuproplus <maxwupro@outlook.com>
2021-12-07 12:27:56 +0800

    update stableswap function

--- src/components/stableswap/StableSwap.tsx
+++ src/components/stableswap/StableSwap.tsx
@@ -4,11 +4,14 @@ import { ConnectToNearBtn, SolidButton } from '~components/button/Button';
 import InputAmount from '~components/forms/InputAmount';
 import SlippageSelector from '~components/forms/SlippageSelector';
 import SubmitButton from '~components/forms/SubmitButton';
-import { TokenMetadata } from '~services/ft-contract';
+import { TokenMetadata, ftGetBalance } from '~services/ft-contract';
 import { wallet } from '~services/near';
+import { swap } from '~services/stable-swap';
 import { TokenBalancesView } from '~services/token';
 import { useStableSwap, useSwap } from '~state/swap';
 import { isMobile } from '~utils/device';
+import Alert from '../alert/Alert';
+
 import {
   calculateExchangeRate,
   toPrecision,
@@ -21,25 +24,34 @@ interface StableSwapProps {
   tokens: TokenMetadata[];
 }
 const SWAP_SLIPPAGE_KEY = 'REF_FI_STABLE_SWAP_SLIPPAGE_VALUE';
+export const SWAP_USE_NEAR_BALANCE_KEY = 'REF_FI_USE_NEAR_BALANCE_VALUE';
 export default function StableSwap({ tokens, balances }: StableSwapProps) {
   const [tokenIn, setTokenIn] = useState<TokenMetadata>(tokens[0]);
   const [tokenOut, setTokenOut] = useState<TokenMetadata>(tokens[1]);
   const [tokenInAmount, setTokenInAmount] = useState<string>('1');
   const [slippageTolerance, setSlippageTolerance] = useState<number>(0.5);
   const [disabled, setDisabled] = useState<boolean>(false);
+  const [useNearBalance, setUseNearBalance] = useState<boolean>(
+    localStorage.getItem(SWAP_USE_NEAR_BALANCE_KEY) != 'false'
+  );
+  const [tokenInBalanceFromNear, setTokenInBalanceFromNear] =
+    useState<string>();
+  const [tokenOutBalanceFromNear, setTokenOutBalanceFromNear] =
+    useState<string>();
   const intl = useIntl();
-  const bindUseBalance = () => {};
   const onChangeSlip = (slippage: number) => {
     setSlippageTolerance(slippage);
     localStorage.setItem(SWAP_SLIPPAGE_KEY, slippage?.toString());
   };
 
-  const { tokenOutAmount, pool, minAmountOut, canSwap } = useStableSwap({
-    tokenIn,
-    tokenInAmount: tokenInAmount,
-    tokenOut,
-    slippageTolerance,
-  });
+  const { tokenOutAmount, pool, canSwap, minAmountOut, swapError, submit } =
+    useStableSwap({
+      tokenIn,
+      tokenInAmount: tokenInAmount,
+      tokenOut,
+      slippageTolerance,
+      useNearBalance,
+    });
 
   const handleSwapFrom = (tokenFrom: string) => {
     setTokenIn(tokens.filter((item) => item.id === tokenFrom)[0]);
@@ -47,14 +59,35 @@ export default function StableSwap({ tokens, balances }: StableSwapProps) {
   const handleSwapTo = (tokenTo: string) => {
     setTokenOut(tokens.filter((item) => item.id === tokenTo)[0]);
   };
-  const handleSubmit = () => {
-    event.preventDefault();
 
-    if (wallet.isSignedIn()) {
-      try {
-      } catch (err) {}
+  useEffect(() => {
+    if (useNearBalance) {
+      if (tokenIn) {
+        const tokenInId = tokenIn.id;
+        if (tokenInId) {
+          if (wallet.isSignedIn()) {
+            ftGetBalance(tokenInId).then((available) =>
+              setTokenInBalanceFromNear(
+                toReadableNumber(tokenIn?.decimals, available)
+              )
+            );
+          }
+        }
+      }
+      if (tokenOut) {
+        const tokenOutId = tokenOut.id;
+        if (tokenOutId) {
+          if (wallet.isSignedIn()) {
+            ftGetBalance(tokenOutId).then((available) =>
+              setTokenOutBalanceFromNear(
+                toReadableNumber(tokenOut?.decimals, available)
+              )
+            );
+          }
+        }
+      }
     }
-  };
+  }, [tokenIn, tokenOut, useNearBalance]);
 
   useEffect(() => {
     const rememberedSlippageTolerance =
@@ -63,17 +96,28 @@ export default function StableSwap({ tokens, balances }: StableSwapProps) {
     setSlippageTolerance(Number(rememberedSlippageTolerance));
   }, []);
 
+  const tokenInMax = useNearBalance
+    ? tokenInBalanceFromNear || '0'
+    : toReadableNumber(tokenIn?.decimals, balances?.[tokenIn?.id]) || '0';
+  const tokenOutTotal = useNearBalance
+    ? tokenOutBalanceFromNear || '0'
+    : toReadableNumber(tokenOut?.decimals, balances?.[tokenOut?.id]) || '0';
+  const canSubmit = canSwap && (tokenInMax != '0' || !useNearBalance);
+
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="overflow-y-auto bg-secondary shadow-2xl rounded-2xl py-6 bg-dark xs:rounded-lg md:rounded-lg"
-    >
+    <form className="overflow-y-auto bg-secondary shadow-2xl rounded-2xl py-6 bg-dark xs:rounded-lg md:rounded-lg">
       <div className="formTitle flex justify-between text-xl text-white text-left px-8">
         <FormattedMessage id="stable_swap" defaultMessage="StableSwap" />
         <SlippageSelector
           slippageTolerance={slippageTolerance}
           onChange={onChangeSlip}
-          bindUseBalance={bindUseBalance}
+          bindUseBalance={(useNearBalance) => {
+            setUseNearBalance(useNearBalance);
+            localStorage.setItem(
+              SWAP_USE_NEAR_BALANCE_KEY,
+              useNearBalance.toString()
+            );
+          }}
         />
       </div>
       <div className="flex mt-6 px-8">
@@ -82,11 +126,7 @@ export default function StableSwap({ tokens, balances }: StableSwapProps) {
             From:{' '}
             <span className="float-right">
               <FormattedMessage id="balance" defaultMessage="Balance" />: &nbsp;
-              {toPrecision(
-                toReadableNumber(tokenIn.decimals, balances[tokenIn.id]),
-                2,
-                true
-              )}
+              {toPrecision(tokenInMax, 3)}
             </span>
           </p>
           <InputAmount
@@ -98,7 +138,7 @@ export default function StableSwap({ tokens, balances }: StableSwapProps) {
               setTokenInAmount(amount);
             }}
             disabled={disabled}
-            max={toReadableNumber(tokenIn.decimals, balances[tokenIn.id])}
+            max={tokenInMax}
           />
         </div>
 
@@ -114,11 +154,7 @@ export default function StableSwap({ tokens, balances }: StableSwapProps) {
             To:{' '}
             <span className=" float-right">
               <FormattedMessage id="balance" defaultMessage="Balance" />: &nbsp;
-              {toPrecision(
-                toReadableNumber(tokenOut.decimals, balances[tokenOut.id]),
-                2,
-                true
-              )}
+              {toPrecision(tokenOutTotal, 3)}
             </span>
           </p>
           <InputAmount
@@ -127,6 +163,7 @@ export default function StableSwap({ tokens, balances }: StableSwapProps) {
             disabled={true}
             name={tokenOut?.id}
             value={tokenOutAmount}
+            max={tokenOutTotal}
           />
         </div>
       </div>
@@ -150,9 +187,23 @@ export default function StableSwap({ tokens, balances }: StableSwapProps) {
           canSwap={canSwap}
         />
       </div>
+      <div className="mx-8">
+        <div className="pb-2">
+          {swapError && <Alert level="error" message={swapError.message} />}
+        </div>
+      </div>
+
       <div className="mx-8 mt-8">
         {wallet.isSignedIn() ? (
-          <SolidButton className="w-full text-lg">
+          <SolidButton
+            className="w-full text-lg"
+            disabled={!canSubmit}
+            onClick={(e) => {
+              e.preventDefault();
+
+              submit();
+            }}
+          >
             <FormattedMessage id="swap" defaultMessage="Swap" />
           </SolidButton>
         ) : (
