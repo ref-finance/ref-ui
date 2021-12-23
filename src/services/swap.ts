c7e774522f71c61278f294f3c496651b65957d68
maxwuproplus <maxwupro@outlook.com>
2021-12-23 10:53:16 +0800

    print swap log

--- src/services/swap.ts
+++ src/services/swap.ts
@@ -122,12 +122,16 @@ export const estimateSwap = async ({
     .slice(0, MAXIMUM_NUMBER_OF_POOLS)
     .filter(filterFunc);
 
+  console.log('filtered pools', filteredPools, 'pools', pools);
+
   const poolAllocations = calculateOptimalOutput(
     filteredPools,
     parsedAmountIn,
     tokenIn.id,
     tokenOut.id
   );
+
+  console.log('allocations', poolAllocations);
 
   const parallelPoolsWithAllocation = filteredPools.map((pool, i) => ({
     ...pool,
