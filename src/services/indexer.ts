29f79a3c328c25edd41c442d8e1b0e131dee7f65
Max <maxwupro@outlook.com>
2022-01-17 01:12:00 +0800

    remove pool list log

--- src/services/indexer.ts
+++ src/services/indexer.ts
@@ -74,14 +74,12 @@ export const getTopPools = async (): Promise<PoolRPCView[]> => {
 
     if (await db.checkTopPools()) {
       pools = await db.queryTopPools();
-      console.log('pools from cache', pools);
     } else {
       pools = await fetch(config.indexerUrl + '/list-top-pools', {
         method: 'GET',
         headers: { 'Content-type': 'application/json; charset=UTF-8' },
       }).then((res) => res.json());
       await db.cacheTopPools(pools);
-      console.log('pools from indexer', pools);
     }
 
     pools = pools.map((pool: any) => parsePoolView(pool));
