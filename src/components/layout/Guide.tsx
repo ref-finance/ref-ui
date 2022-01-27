a05dd7c8476dfcd02c94d7f786d80ac4c67bbb18
nature.xie <nature@ref.finance>
2022-01-27 11:16:03 +0800

    add guide box for swap page

--- src/components/layout/Guide.tsx
+++ src/components/layout/Guide.tsx
@@ -46,57 +46,116 @@ export function Guide(props: any) {
   return (
     <>
       {!closeStatus ? (
-        <div className="fixed right-20 top-20 z-10 xs:-mt-6 md:-mt-6 xs:relative md:relative xs:right-2 md:right-2 xs:-top-2 md:-top-2 xs:h-28 md:h-28 xs:overflow-hidden md:overflow-hidden">
-          <div className="absolute right-20 top-5">
-            <TipBoxBig></TipBoxBig>
-            <ModalClose
-              onClick={closePop}
-              width="8"
-              height="8"
-              className="absolute right-5 top-3 cursor-pointer"
-            ></ModalClose>
-            <span className="absolute top-2.5 left-2.5 w-54 text-sm text-white">
-              Go to your account to{' '}
-              <label className="text-greenColor">deposit</label> &{' '}
-              <label className="text-greenColor">withdraw</label>
-            </span>
-            <div
-              onClick={closePop}
-              className="cursor-pointer absolute bottom-2.5 right-12 border border-greenColor rounded-3xl text-xs text-greenColor px-5 py-0.5"
-            >
-              Got it
+        <>
+          {mobile ? (
+            <div className="flex justify-between items-end -mt-6 relative w-full h-28">
+              <div className="relative mb-6">
+                <TipBoxBig></TipBoxBig>
+                <ModalClose
+                  onClick={closePop}
+                  width="8"
+                  height="8"
+                  className="absolute right-5 top-3 cursor-pointer"
+                ></ModalClose>
+                <span className="absolute top-2.5 left-2.5 w-54 text-sm text-white">
+                  Go to your account to{' '}
+                  <label className="text-greenColor">deposit</label> &{' '}
+                  <label className="text-greenColor">withdraw</label>
+                </span>
+                <div
+                  onClick={closePop}
+                  className="cursor-pointer absolute bottom-2.5 right-12 border border-greenColor rounded-3xl text-xs text-greenColor px-5 py-0.5"
+                >
+                  Got it
+                </div>
+              </div>
+              <div className="mb-2">
+                {' '}
+                <RefGuideIcon></RefGuideIcon>
+              </div>
             </div>
-          </div>
-          <div className="xs:float-right md:float-right">
-            {' '}
-            <RefGuideIcon></RefGuideIcon>
-          </div>
-        </div>
+          ) : (
+            <div className="fixed right-20 top-20 z-20">
+              <div className="absolute right-20 top-5">
+                <TipBoxBig></TipBoxBig>
+                <ModalClose
+                  onClick={closePop}
+                  width="8"
+                  height="8"
+                  className="absolute right-5 top-3 cursor-pointer"
+                ></ModalClose>
+                <span className="absolute top-2.5 left-2.5 w-54 text-sm text-white">
+                  Go to your account to{' '}
+                  <label className="text-greenColor">deposit</label> &{' '}
+                  <label className="text-greenColor">withdraw</label>
+                </span>
+                <div
+                  onClick={closePop}
+                  className="cursor-pointer absolute bottom-2.5 right-12 border border-greenColor rounded-3xl text-xs text-greenColor px-5 py-0.5"
+                >
+                  Got it
+                </div>
+              </div>
+              <div>
+                {' '}
+                <RefGuideIcon></RefGuideIcon>
+              </div>
+            </div>
+          )}
+        </>
       ) : null}
       {!closeStatusSet ? (
-        <div className="absolute right-20 -top-12 z-50 xs:z-0 md:z-0 xs:-mt-6 md:-mt-6 xs:flex md:flex xs:justify-end md:justify-end xs:items-end md:items-end xs:right-2 md:right-2 xs:top-0 md:top-0 xs:relative md:relative xs:h-28 md:h-28">
-          <div className="absolute right-20 -top-10 xs:relative md:relative xs:right-0 md:right-0 xs:-top-5 md:top-top-5">
-            <TipBox></TipBox>
-            <ModalClose
-              onClick={closeSetPop}
-              width="8"
-              height="8"
-              className="absolute right-5 top-3 cursor-pointer"
-            ></ModalClose>
-            <span className="absolute top-2 left-2.5 w-54 text-sm text-white">
-              Check the transaction settings
-            </span>
-            <div
-              onClick={closeSetPop}
-              className="cursor-pointer absolute bottom-2 right-10 border border-greenColor rounded-3xl text-xs text-greenColor px-5 py-0.5"
-            >
-              Got it
+        <>
+          {mobile ? (
+            <div className="flex w-full justify-between items-end relative h-28 -mt-6">
+              <div className="relative mb-5">
+                <TipBox></TipBox>
+                <ModalClose
+                  onClick={closeSetPop}
+                  width="8"
+                  height="8"
+                  className="absolute right-5 top-3 cursor-pointer"
+                ></ModalClose>
+                <span className="absolute top-2 left-2.5 w-54 text-sm text-white">
+                  Check the transaction settings
+                </span>
+                <div
+                  onClick={closeSetPop}
+                  className="cursor-pointer absolute bottom-2 right-10 border border-greenColor rounded-3xl text-xs text-greenColor px-5 py-0.5"
+                >
+                  Got it
+                </div>
+              </div>
+              <div className="-mb-6">
+                <RefSetPeople></RefSetPeople>
+              </div>
             </div>
-          </div>
-          <div className="xs:ml-3 md:ml-3">
-            <RefSetPeople></RefSetPeople>
-          </div>
-        </div>
+          ) : (
+            <div className="absolute right-2 -top-12 z-50">
+              <div className="absolute right-20 -top-10">
+                <TipBox></TipBox>
+                <ModalClose
+                  onClick={closeSetPop}
+                  width="8"
+                  height="8"
+                  className="absolute right-5 top-3 cursor-pointer"
+                ></ModalClose>
+                <span className="absolute top-2 left-2.5 w-54 text-sm text-white">
+                  Check the transaction settings
+                </span>
+                <div
+                  onClick={closeSetPop}
+                  className="cursor-pointer absolute bottom-2 right-10 border border-greenColor rounded-3xl text-xs text-greenColor px-5 py-0.5"
+                >
+                  Got it
+                </div>
+              </div>
+              <div className="xs:ml-3 md:ml-3">
+                <RefSetPeople></RefSetPeople>
+              </div>
+            </div>
+          )}
+        </>
       ) : null}
     </>
   );
