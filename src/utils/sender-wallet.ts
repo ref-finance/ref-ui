e598208da2f1a63a4ab25a8f3c7962ee68d65ed5
Max <maxwupro@outlook.com>
2022-02-25 22:52:03 +0800

    add event on accountChanged

--- src/utils/sender-wallet.ts
+++ src/utils/sender-wallet.ts
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { REF_FARM_CONTRACT_ID } from '../services/near';
+import { REF_FARM_CONTRACT_ID, wallet } from '../services/near';
 
 export const SENDER_WALLET_SIGNEDIN_STATE_KEY =
   'SENDER_WALLET_SIGNEDIN_STATE_VALUE';
@@ -24,18 +24,30 @@ senderWalletFunc.prototype = senderWalletExtention;
 const senderWallet = new (senderWalletFunc as any)();
 
 export const useSenderWallet = () => {
+  const getAccountName = (accountId: string = senderWallet.getAccountId()) => {
+    const [account, network] = accountId.split('.');
+    const niceAccountId = `${account.slice(0, 10)}...${network || ''}`;
+
+    return account.length > 10 ? niceAccountId : accountId;
+  };
+
   const [isSignedIn, setIsSignedIn] = useState<boolean>(
     senderWallet.isSignedIn()
   );
-  const [account, network] = senderWallet.getAccountId().split('.');
 
-  const niceAccountId = `${account.slice(0, 10)}...${network || ''}`;
-  const accountName =
-    account.length > 10 ? niceAccountId : senderWallet.getAccountId();
+  const [accountName, setAccountName] = useState<string>('');
+
+  useEffect(() => {
+    setAccountName(getAccountName());
+  }, [isSignedIn]);
 
   senderWallet.on({
     signIn: () => setIsSignedIn(true),
     signOut: () => setIsSignedIn(false),
+    accountChanged: (changedAccountId: string) => {
+      senderWallet.requestSignIn(REF_FARM_CONTRACT_ID);
+      setAccountName(getAccountName(changedAccountId));
+    },
   });
 
   return {
