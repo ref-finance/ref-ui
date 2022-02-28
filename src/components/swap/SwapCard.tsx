e48a3f193e72502db690556d299d1e2c21c0a77a
Joe Zhu <aidai524@gmail.com>
2022-02-28 12:18:23 +0800

    Update smart routing algorithm (#232)
    * upgrade smart routing V1 algorithm
    
    * fix calculation delay pb
    
    * print log
    
    * fix price impact pb
    
    * fix estimates delay
    
    * fix exchange rate bug
    
    * fix swap rate delay
    
    * fix pool fee and price impact value delay
    
    * fix price impact diplay error
    
    * update swap detail number precision
    
    * fix price impact calculation delay
    
    * remove log
    
    Co-authored-by: Max <maxwupro@outlook.com>

--- src/components/swap/SwapCard.tsx
+++ src/components/swap/SwapCard.tsx
@@ -226,7 +226,7 @@ export function SwapRateDetail({
     if (ONLY_ZEROS.test(fromNow)) return '-';
 
     return calculateExchangeRate(fee, fromNow, toNow);
-  }, [isRevert, to, from]);
+  }, [isRevert, to]);
 
   useEffect(() => {
     setNewValue(value);
@@ -240,7 +240,7 @@ export function SwapRateDetail({
         isRevert ? tokenOut.symbol : tokenIn.symbol
       )}`
     );
-  }, [isRevert]);
+  }, [isRevert, exchangeRageValue]);
 
   function switchSwapRate() {
     setIsRevert(!isRevert);
@@ -500,6 +500,20 @@ function DetailView({
     if (swapsTodo?.length > 1) {
       setShowDetails(true);
     }
+  }, [swapsTodo]);
+
+  const priceImpactDisplay = useMemo(() => {
+    if (!priceImpact || !tokenIn || !from) return null;
+    return GetPriceImpact(priceImpact, tokenIn, from);
+  }, [to, priceImpact]);
+
+  const poolFeeDisplay = useMemo(() => {
+    if (!fee || !from || !tokenIn) return null;
+
+    return `${toPrecision(
+      calculateFeePercent(fee).toString(),
+      2
+    )}% / ${calculateFeeCharge(fee, from)} ${toRealSymbol(tokenIn.symbol)}`;
   }, [to]);
 
   if (!pools || ONLY_ZEROS.test(from) || !to || tokenIn.id === tokenOut.id)
@@ -546,18 +560,11 @@ function DetailView({
         )}
         <SwapDetail
           title={intl.formatMessage({ id: 'price_impact' })}
-          value={
-            !to || to === '0' ? '-' : GetPriceImpact(priceImpact, tokenIn, from)
-          }
+          value={!to || to === '0' ? '-' : priceImpactDisplay}
         />
         <SwapDetail
           title={intl.formatMessage({ id: 'pool_fee' })}
-          value={`${toPrecision(
-            calculateFeePercent(fee).toString(),
-            2
-          )}% / ${calculateFeeCharge(fee, from)} ${toRealSymbol(
-            tokenIn.symbol
-          )}`}
+          value={poolFeeDisplay}
         />
 
         {isParallelSwap && pools.length > 1 && (
@@ -681,7 +688,7 @@ export default function SwapCard(props: { allTokens: TokenMetadata[] }) {
   const priceImpactValueParallelSwap = useMemo(() => {
     if (!pools || !tokenOutAmount || !tokenInAmount) return '0';
     return calculatePriceImpact(pools, tokenIn, tokenOut, tokenInAmount);
-  }, [tokenOutAmount]);
+  }, [swapsToDo]);
 
   const priceImpactValueSmartRouting = useMemo(() => {
     {
@@ -694,7 +701,7 @@ export default function SwapCard(props: { allTokens: TokenMetadata[] }) {
         tokenOut
       );
     }
-  }, [tokenOutAmount]);
+  }, [swapsToDo]);
 
   const PriceImpactValue = isParallelSwap
     ? priceImpactValueParallelSwap
