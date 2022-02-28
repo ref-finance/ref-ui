09c2847e58f7893382767823edc5987ae6466844
Max <maxwupro@outlook.com>
2022-02-28 19:25:45 +0800

    add simple wallet selector

---
+++ src/components/layout/WalletSelector.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import ReactModal from 'react-modal';
+import { Card } from '../card/Card';
+import { wallet, REF_FARM_CONTRACT_ID } from '../../services/near';
+import { senderWallet } from '../../utils/sender-wallet';
+import Modal from 'react-modal';
+
+export const WalletSelectorModal = (
+  props: ReactModal.Props & { setShowWalletSelector: (show: boolean) => void }
+) => {
+  const { setShowWalletSelector } = props;
+
+  return (
+    <Modal {...props}>
+      <Card
+        className="p-5 flex flex-col"
+        style={{
+          width: '40vw',
+        }}
+      >
+        <div
+          className="m-5 w-full cursor-pointer text-white"
+          onClick={() => {
+            wallet.requestSignIn(REF_FARM_CONTRACT_ID);
+          }}
+        >
+          web wallet
+        </div>
+
+        <div
+          className="m-5 w-full cursor-pointer text-white"
+          onClick={() => {
+            senderWallet.requestSignIn(REF_FARM_CONTRACT_ID).then(() => {
+              setShowWalletSelector(false);
+            });
+          }}
+        >
+          sender wallet
+        </div>
+      </Card>
+    </Modal>
+  );
+};
