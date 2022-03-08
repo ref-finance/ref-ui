f461440cd630ae1b17c7d133330a48d5f8863cee
Max <max@ref.finance>
2022-03-08 22:43:34 +0800

    fix allocation detail UI bug

--- src/components/swap/SwapCard.tsx
+++ src/components/swap/SwapCard.tsx
@@ -321,12 +321,18 @@ export function ParallelSwapRoutesDetail({
 
       const sortedAmount = sortBy(partialAmounts, (p) => Number(p));
 
+      let minIndexes: number[] = [];
+
       for (let k = 0; k < sortedAmount.length - 1; k++) {
         let minIndex = -1;
 
         for (let j = 0; j < partialAmounts.length; j++) {
-          if (partialAmounts[j].eq(sortedAmount[k])) {
+          if (
+            partialAmounts[j].eq(sortedAmount[k]) &&
+            !minIndexes.includes(j)
+          ) {
             minIndex = j;
+            minIndexes.push(j);
             break;
           }
         }
