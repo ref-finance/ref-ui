1b0862c4a73be9411428d3d1ed4fc0334f3ccf61
Joe <joe@ref.finance>
2022-03-30 19:17:01 +0800

    add Russian lang

--- src/utils/menu.tsx
+++ src/utils/menu.tsx
@@ -142,6 +142,14 @@ export const useMenuItems = () => {
           id: 14,
           logo: <UkIcon />,
         },
+        {
+          label: 'Pусский',
+          isExternal: false,
+          language: 'ru',
+          url: '',
+          id: 15,
+          logo: <RuIcon />,
+        },
       ],
     },
   ];
