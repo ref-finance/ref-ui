9b6d1d4570cefbbf3396ef6893a7409522417c00
Max <max@ref.finance>
2022-04-14 17:05:22 +0800

    add slippage error tip on stable swap

--- src/state/swap.tsx
+++ src/state/swap.tsx
@@ -119,8 +119,6 @@ export const useSwap = ({
 
   useEffect(() => {
     if (txHash && getCurrentWallet().wallet.isSignedIn()) {
-      console.log(txHash);
-
       checkTransaction(txHash)
         .then((res: any) => {
           const slippageErrorPattern = /ERR_MIN_AMOUNT|slippage error/i;
@@ -331,22 +329,36 @@ export const useStableSwap = ({
   };
 
   useEffect(() => {
-    if (txHash) {
+    if (txHash && getCurrentWallet().wallet.isSignedIn()) {
       checkTransaction(txHash)
-        .then(({ transaction }) => {
-          return (
-            transaction?.actions[1]?.['FunctionCall']?.method_name ===
-              'ft_transfer_call' ||
-            transaction?.actions[0]?.['FunctionCall']?.method_name ===
-              'ft_transfer_call' ||
-            transaction?.actions[0]?.['FunctionCall']?.method_name === 'swap' ||
-            transaction?.actions[0]?.['FunctionCall']?.method_name ===
-              'near_withdraw'
-          );
+        .then((res: any) => {
+          const slippageErrorPattern = /ERR_MIN_AMOUNT|slippage error/i;
+
+          const isSlippageError = res.receipts_outcome.some((outcome: any) => {
+            return slippageErrorPattern.test(
+              outcome?.outcome?.status?.Failure?.ActionError?.kind
+                ?.FunctionCallError?.ExecutionError
+            );
+          });
+
+          const transaction = res.transaction;
+          return {
+            isSwap:
+              transaction?.actions[1]?.['FunctionCall']?.method_name ===
+                'ft_transfer_call' ||
+              transaction?.actions[0]?.['FunctionCall']?.method_name ===
+                'ft_transfer_call' ||
+              transaction?.actions[0]?.['FunctionCall']?.method_name ===
+                'swap' ||
+              transaction?.actions[0]?.['FunctionCall']?.method_name ===
+                'near_withdraw',
+            isSlippageError,
+          };
         })
-        .then((isSwap) => {
+        .then(({ isSwap, isSlippageError }) => {
           if (isSwap) {
-            !errorType && swapToast(txHash);
+            !isSlippageError && !errorType && swapToast(txHash);
+            isSlippageError && failToast(txHash, 'Slippage Violation');
           }
           history.replace(pathname);
         });
