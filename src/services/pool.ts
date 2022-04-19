631d137185dac5fd2b22b4a19adb9c709be920d5
Max <max@ref.finance>
2022-04-19 15:14:33 +0800

    add tri pools log

--- src/services/pool.ts
+++ src/services/pool.ts
@@ -303,6 +303,8 @@ export const getPoolsByTokens = async ({
       triPools = await getAllTriPools();
     }
 
+    console.log('aurora pools', triPools);
+
     filtered_pools = pools
       .concat(triPools || [])
       .filter(isNotStablePool)
