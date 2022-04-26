427cb8e119e9bf936c6121d334bb6288133fae57
nature.xie <nature@ref.finance>
2022-04-26 15:14:27 +0800

    modify testnet config

--- src/services/config.ts
+++ src/services/config.ts
@@ -108,27 +108,27 @@ export default function getConfig(env: string = process.env.NEAR_ENV) {
         walletUrl: 'https://wallet.testnet.near.org',
         helperUrl: 'https://helper.testnet.near.org',
         explorerUrl: 'https://testnet.nearblocks.io',
-        indexerUrl: 'https://testnet-indexer.ref-finance.com',
+        indexerUrl: 'https://dev-indexer.ref-finance.com',
         sodakiApiUrl: 'https://api.stats.ref.finance/api',
         blackList: process.env.FARM_BLACK_LIST || ['1371#3'],
         REF_FI_CONTRACT_ID:
-          process.env.REF_FI_CONTRACT_ID || 'ref-finance-101.testnet',
+          process.env.REF_FI_CONTRACT_ID || 'exchange.ref-dev.testnet',
         WRAP_NEAR_CONTRACT_ID:
           process.env.WRAP_NEAR_CONTRACT_ID || 'wrap.testnet',
         REF_ADBOARD_CONTRACT_ID: 'ref-adboard.near',
         REF_FARM_CONTRACT_ID:
-          process.env.REF_FARM_CONTRACT_ID || 'v2.ref-farming.testnet',
+          process.env.REF_FARM_CONTRACT_ID || 'farm110.ref-dev.testnet',
         REF_TOKEN_ID: 'ref.fakes.testnet',
-        XREF_TOKEN_ID: 'xref.ref-finance.testnet',
+        XREF_TOKEN_ID: 'xref.ref-dev.testnet',
         REF_AIRDROP_CONTRACT_ID: 'locker002.ref-dev.testnet',
         TOP_POOLS_TOKEN_REFRESH_INTERVAL:
           process.env.POOL_TOKEN_REFRESH_INTERVAL || 60,
         POOL_TOKEN_REFRESH_INTERVAL:
           process.env.POOL_TOKEN_REFRESH_INTERVAL || 20,
-        STABLE_POOL_ID: process.env.STABLE_POOL_ID || 218,
+        STABLE_POOL_ID: process.env.STABLE_POOL_ID || 79,
         STABLE_POOL_IDS: process.env.STABLE_POOL_IDS || ['218', '3020'],
 
-        STABLE_POOL_USN_ID: process.env.STABLE_POOL_USN_ID || 356,
+        STABLE_POOL_USN_ID: process.env.STABLE_POOL_USN_ID || 506,
         STABLE_TOKEN_IDS: [
           'usdt.fakes.testnet',
           'usdc.fakes.testnet',
@@ -136,7 +136,7 @@ export default function getConfig(env: string = process.env.NEAR_ENV) {
         ],
         STABLE_TOKEN_USN_IDS: ['usdn.testnet', 'usdt.fakes.testnet'],
         STABLE_TOKEN_USN_INDEX: {
-          'usdn.testnet': 0,
+          'usn.fakes.testnet': 0,
           'usdt.fakes.testnet': 1,
         },
         STABLE_TOKEN_INDEX: {
