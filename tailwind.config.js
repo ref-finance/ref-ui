1d466e125c1674654b8470f3d4fb8c0422c2045c
Max <max@ref.finance>
2022-04-28 10:54:24 +0800

    update screen width

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      xs: { min: '300px', max: '599px' },
+      xs: { min: '300px', max: '600px' },
       md: { min: '600px', max: '1023px' },
       lg: { min: '1024px' },
       xl: { min: '1280px' },
