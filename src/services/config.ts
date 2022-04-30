11ea1048d062b4463e36b14d8f3390573281494b
nature.xie <nature@ref.finance>
2022-04-30 22:59:20 +0800

    update testnet config

--- src/services/config.ts
+++ src/services/config.ts
@@ -127,17 +127,17 @@ export default function getConfig(env: string = process.env.NEAR_ENV) {
         POOL_TOKEN_REFRESH_INTERVAL:
           process.env.POOL_TOKEN_REFRESH_INTERVAL || 20,
         STABLE_POOL_ID: process.env.STABLE_POOL_ID || 79,
-        STABLE_POOL_IDS: process.env.STABLE_POOL_IDS || ['79', '506'],
+        STABLE_POOL_IDS: process.env.STABLE_POOL_IDS || ['79', '603'],
         USN_ID: 'usdn.testnet',
-        STABLE_POOL_USN_ID: process.env.STABLE_POOL_USN_ID || 506,
+        STABLE_POOL_USN_ID: process.env.STABLE_POOL_USN_ID || 603,
         STABLE_TOKEN_IDS: [
           'usdt.fakes.testnet',
           'usdc.fakes.testnet',
           'dai.fakes.testnet',
         ],
-        STABLE_TOKEN_USN_IDS: ['usn.fakes.testnet', 'usdt.fakes.testnet'],
+        STABLE_TOKEN_USN_IDS: ['usdn.testnet', 'usdt.fakes.testnet'],
         STABLE_TOKEN_USN_INDEX: {
-          'usn.fakes.testnet': 0,
+          'usdn.testnet': 0,
           'usdt.fakes.testnet': 1,
         },
         STABLE_TOKEN_INDEX: {
