240315e3d66eb70f17d9af615e581e5c47c82614
nature.xie <nature@ref.finance>
2022-07-11 14:29:31 +0800

    simplify the pool tab

--- src/components/pool/PoolTab.tsx
+++ src/components/pool/PoolTab.tsx
@@ -27,13 +27,17 @@ export const PoolTab = () => {
   }
   return (
     <div
-      className="flex items-center justify-between rounded-2xl bg-cardBg text-primaryText font-normal mb-4 text-lg  p-1 xl:w-1/3 2xl:w-1/3 3xl:w-1/4 lg:w-1/2 md:w-5/6 mx-auto  xs:text-sm md:text-sm xs:w-auto xs:mx-2"
+      className={`flex items-center justify-between rounded-2xl bg-cardBg text-primaryText font-normal mb-4 text-lg  p-1 mx-auto  xs:text-sm md:text-sm ${
+        isSignedIn
+          ? 'xl:w-1/3 2xl:w-1/3 3xl:w-1/4 lg:w-1/2 md:w-5/6 xs:w-auto xs:mx-2'
+          : 'xl:w-1/4 2xl:w-1/4 3xl:w-1/4 lg:w-1/3 md:w-5/6 xs:w-5/6'
+      }`}
       style={{
         height: '50px',
       }}
     >
       <span
-        className={`flex items-center justify-center h-full  flex-grow text-center cursor-pointer ${
+        className={`flex items-center justify-center h-full flex-grow text-center cursor-pointer ${
           choosedTab == '1' ? 'bg-tabChosen text-white rounded-xl' : ''
         }`}
         onClick={() => {
@@ -42,7 +46,7 @@ export const PoolTab = () => {
       >
         <FormattedMessage id="view_pools" defaultMessage="View Pools" />
       </span>
-      {
+      {/* {
         <span
           className={`flex items-center justify-center h-full  flex-grow text-center cursor-pointer ${
             choosedTab == '2' ? 'bg-tabChosen text-white rounded-xl' : ''
@@ -56,7 +60,7 @@ export const PoolTab = () => {
             defaultMessage="Create New Pool"
           />
         </span>
-      }
+      } */}
 
       {isSignedIn ? (
         <span
