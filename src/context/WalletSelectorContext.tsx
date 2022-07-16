96b70356ca7b679f183d1be968bd46477e865adc
Max <max@ref.finance>
2022-07-16 22:08:20 +0800

    fix wallet-connect bug

--- src/context/WalletSelectorContext.tsx
+++ src/context/WalletSelectorContext.tsx
@@ -10,7 +10,7 @@ import { setupSender } from '@near-wallet-selector/sender';
 import { setupMathWallet } from '@near-wallet-selector/math-wallet';
 import { setupNightly } from '@near-wallet-selector/nightly';
 import { setupLedger } from '@near-wallet-selector/ledger';
-import { setupWalletConnect } from '@near-wallet-selector/wallet-connect';
+import { setupWalletConnect } from '@near-wallet-selector/wallet-connect/src';
 import { setupNightlyConnect } from '@near-wallet-selector/nightly-connect';
 
 import getConfig from '../services/config';
