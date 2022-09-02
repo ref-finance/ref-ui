2683eb9afe68c767119071eddbf4bd96af0d3c35
nature.xie <nature@ref.finance>
2022-09-02 09:29:30 +0800

    update farm new icon period

--- src/components/farm/FarmsHome.tsx
+++ src/components/farm/FarmsHome.tsx
@@ -2633,7 +2633,7 @@ function FarmView(props: {
     const result = farmList.find((farm: FarmBoost) => {
       const start_at = farm?.terms?.start_at;
       if (start_at == 0) return true;
-      const one_month_seconds = 15 * 24 * 60 * 60;
+      const one_month_seconds = 30 * 24 * 60 * 60;
       const currentA = new Date().getTime();
       const compareB = new BigNumber(start_at)
         .plus(one_month_seconds)
