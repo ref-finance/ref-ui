009bd70ec548b937e501b333acc4f6a63b5615db
Max <max@ref.finance>
2022-09-07 14:46:52 +0800

    add transak transaction pop up

--- src/components/alert/Transak.tsx
+++ src/components/alert/Transak.tsx
@@ -1,6 +1,10 @@
 import transakSDK from '@transak/transak-sdk';
 import { getTransakConfig } from '~services/config';
 import { isClientMobie } from '../../utils/device';
+import {
+  normalFailToast,
+  normalSuccessToast,
+} from '../layout/transactionTipPopUp';
 
 export function openTransak(accountId: string) {
   const isMobile = isClientMobie();
@@ -19,16 +23,17 @@ export function openTransak(accountId: string) {
     transak.close();
   });
   transak.on(transak.EVENTS.TRANSAK_ORDER_FAILED, () => {
-    console.log('Transaction order was failed');
+    normalFailToast('Transaction order was failed');
   });
   transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (successData: any) => {
     const fiatCurrency = successData?.status?.fiatCurrency;
     const fiatAmount = successData?.status?.fiatAmount;
     const cryptoCurrency = successData?.status?.cryptoCurrency;
     const cryptoAmount = successData?.status?.cryptoAmount;
-    console.log(
+    normalSuccessToast(
       `Transaction order to buy from ${fiatAmount} ${fiatCurrency} to ${cryptoAmount} ${cryptoCurrency} was successfully, please wait for 1-3 minutes for completing the order`
     );
+
     transak.close();
   });
 }
--- src/components/layout/transactionTipPopUp.tsx
+++ src/components/layout/transactionTipPopUp.tsx
@@ -490,3 +490,65 @@ export const getErrorMessage = (res: any) => {
     return null;
   }
 };
+
+export const normalFailToast = (text: string) => {
+  toast(
+    <div
+      className="text-error w-full h-full pl-1.5 py-1  flex-col text-sm"
+      style={{
+        lineHeight: '40px',
+        width: '280px',
+      }}
+    >
+      <span>{text}</span>
+    </div>,
+    {
+      autoClose: false,
+      closeOnClick: true,
+      hideProgressBar: false,
+      closeButton: <CloseIcon />,
+      progressStyle: {
+        background: '#FF7575',
+        borderRadius: '8px',
+      },
+      style: {
+        background: '#1D2932',
+        boxShadow: '0px 0px 10px 10px rgba(0, 0, 0, 0.15)',
+        border: '1px solid #FF7575',
+        borderRadius: '8px',
+      },
+    }
+  );
+};
+export const normalSuccessToast = (text: string) => {
+  toast(
+    <div
+      className="text-white w-full h-full pl-1.5 text-sm  flex-wrap items-center"
+      style={{
+        lineHeight: '30px',
+        width: '270px',
+      }}
+    >
+      <div className="w-4 h-4 mr-2  relative top-1 inline-flex">
+        <SwapCheckIcon />
+      </div>
+      {text}
+    </div>,
+    {
+      autoClose: 8000,
+      closeOnClick: true,
+      hideProgressBar: false,
+      closeButton: <CloseIcon />,
+      progressStyle: {
+        background: '#00FFD1',
+        borderRadius: '8px',
+      },
+      style: {
+        background: '#1D2932',
+        boxShadow: '0px 0px 10px 10px rgba(0, 0, 0, 0.15)',
+        borderRadius: '8px',
+        minHeight: '0px',
+      },
+    }
+  );
+};
