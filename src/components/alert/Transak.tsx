b78cc53a489f164a6940376575ec09a7a6f28aea
Max <max@ref.finance>
2022-09-12 00:22:57 +0800

    fix scrolling error

--- src/components/alert/Transak.tsx
+++ src/components/alert/Transak.tsx
@@ -6,6 +6,8 @@ import {
   normalSuccessToast,
 } from '../layout/transactionTipPopUp';
 
+const successMarker = 'REF_FI_TRANSAL_TIP';
+
 export function openTransak(accountId: string) {
   const isMobile = isClientMobie();
 
@@ -21,34 +23,39 @@ export function openTransak(accountId: string) {
 
   transak.init();
 
+  transak.on(transak.EVENTS.TRANSAK_WIDGET_OPEN, () => {
+    sessionStorage.setItem(successMarker, 'true');
+  });
+
   transak.on(transak.EVENTS.TRANSAK_WIDGET_CLOSE, () => {
     transak.close();
   });
   transak.on(transak.EVENTS.TRANSAK_ORDER_FAILED, () => {
-    const el = document.getElementsByClassName(
-      'Toastify__toast-container'
-    )?.[0];
+    const el =
+      document.getElementsByClassName('Toastify__toast-container')?.length > 0;
     if (!el) {
       normalFailToast('Transaction order was failed.');
     }
+    document.documentElement.style.overflow = 'scroll';
   });
 
   transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (successData: any) => {
-    console.log(successData, 'data');
-
     const fiatCurrency = successData?.status?.fiatCurrency;
     const fiatAmount = successData?.status?.fiatAmount;
     const cryptoCurrency = successData?.status?.cryptoCurrency;
     const cryptoAmount = successData?.status?.cryptoAmount;
 
-    const el = document.getElementsByClassName(
-      'Toastify__toast-container'
-    )?.[0];
-    if (!el) {
+    const marker = sessionStorage.getItem(successMarker);
+
+    const el =
+      document.getElementsByClassName('Toastify__toast-container')?.length > 0;
+    if (!el && marker) {
       normalSuccessToast(
         `Transaction order to buy from ${fiatAmount} ${fiatCurrency} to ${cryptoAmount} ${cryptoCurrency} was successfully, please wait for 1-3 minutes for completing the order.`
       );
+      sessionStorage.removeItem(successMarker);
     }
+    document.documentElement.style.overflow = 'scroll';
 
     transak.close();
   });
