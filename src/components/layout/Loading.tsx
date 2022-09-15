bfeb798f7d63c2e1d052fd80ef9b17d8e0f49e07
nature.xie <nature@ref.finance>
2022-09-15 19:17:18 +0800

    liqudity page loading optimization

--- src/components/layout/Loading.tsx
+++ src/components/layout/Loading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BeatLoader } from 'react-spinners';
+import { BeatLoader, ClipLoader } from 'react-spinners';
 import Loader from 'react-spinners/ClipLoader';
 // import { Loading } from '~components/icon/Loading';
 import { Loading, RefreshTrangle } from './../icon/Loading';
@@ -10,4 +10,7 @@ export default function () {
 
 export const BeatLoading = () => {
   return <BeatLoader size={5} color="#ffffff" />;
+};
+export const ClipLoadering = () => {
+  return <ClipLoader color="#ffffff" />;
 };
