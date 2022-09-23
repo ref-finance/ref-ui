afd2c7eec35f1060f5fead18ec65348dfff90550
Max <max@ref.finance>
2022-09-23 21:49:17 +0800

    add usdt rated pool

--- src/services/near.ts
+++ src/services/near.ts
@@ -71,9 +71,9 @@ export const {
   NEW_NEARXIDS,
   NEW_NEARX_POOL_ID,
   NEW_NEARX_POOL_INDEX,
-  // USDTIDS,
-  // USDT_POOL_ID,
-  // USDT_POOL_INDEX,
+  USDTIDS,
+  USDT_POOL_ID,
+  USDT_POOL_INDEX,
 } = getExtraStablePoolConfig();
 
 export const extraStableTokenIds = BTCIDS.concat(LINEARIDS)
@@ -81,7 +81,7 @@ export const extraStableTokenIds = BTCIDS.concat(LINEARIDS)
   .concat(NEARXIDS)
   .concat(CUSDIDS)
   .concat(NEW_NEARXIDS)
-  // .concat(USDTIDS)
+  .concat(USDTIDS)
   .filter((_) => !!_);
 
 export const isRatedPool = (id: string | number) => {
@@ -107,7 +107,7 @@ export const ALL_STABLE_POOL_IDS = [
   LINEAR_POOL_ID,
   NEARX_POOL_ID,
   NEW_NEARX_POOL_ID,
-  // USDT_POOL_ID,
+  USDT_POOL_ID,
 ]
   .filter((_) => _)
   .map((id) => id.toString());
@@ -142,8 +142,8 @@ export const getStableTokenIndex = (stable_pool_id: string | number) => {
       return NEARX_POOL_INDEX;
     case NEW_NEARX_POOL_ID:
       return NEW_NEARX_POOL_INDEX;
-    // case USDT_POOL_ID:
-    //   return USDT_POOL_INDEX;
+    case USDT_POOL_ID:
+      return USDT_POOL_INDEX;
   }
 };
 
@@ -170,7 +170,7 @@ export const USD_CLASS_STABLE_POOL_IDS = [
   STABLE_POOL_ID.toString(),
   STABLE_POOL_USN_ID.toString(),
   CUSD_STABLE_POOL_ID,
-  // USDT_POOL_ID,
+  USDT_POOL_ID,
 ];
 
 export const BTC_CLASS_STABLE_TOKEN_IDS = BTCIDS;
@@ -181,8 +181,9 @@ export const NEAR_CLASS_STABLE_TOKEN_IDS = new Array(
 
 export const USD_CLASS_STABLE_TOKEN_IDS = new Array(
   ...new Set(
-    STABLE_TOKEN_USN_IDS.concat(STABLE_TOKEN_IDS).concat(CUSDIDS)
-    // .concat(USDTIDS)
+    STABLE_TOKEN_USN_IDS.concat(STABLE_TOKEN_IDS)
+      .concat(CUSDIDS)
+      .concat(USDTIDS)
   )
 );
 
