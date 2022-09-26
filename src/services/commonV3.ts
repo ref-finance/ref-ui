5947d90c7057f0e3f3977750954916dea5718622
nature.xie <nature@ref.finance>
2022-09-26 13:33:21 +0800

    add chart switch function

--- src/services/commonV3.ts
+++ src/services/commonV3.ts
@@ -4,6 +4,10 @@ import { checkTransaction, checkTransactionStatus } from '../services/swap';
 import { WalletContext } from '../utils/wallets-integration';
 import { useHistory } from 'react-router';
 import BigNumber from 'bignumber.js';
+import * as d3 from 'd3';
+import _ from 'lodash';
+import { toReadableNumber } from '~utils/numbers';
+
 /**
  * caculate price by point
  * @param pointDelta
@@ -139,4 +143,265 @@ export function getYAmount_per_point_by_Ly(L: string, point: number) {
     .multipliedBy(Math.pow(Math.sqrt(CONSTANT_D), point))
     .toFixed();
   return yAmount;
+}
+
+export function drawChartData({
+  depthData,
+  left_point,
+  right_point,
+  chartDom,
+  token_x_decimals,
+  token_y_decimals,
+  sort,
+}: {
+  depthData: any;
+  left_point: number;
+  right_point: number;
+  chartDom: any;
+  token_x_decimals: number;
+  token_y_decimals: number;
+  sort: boolean;
+}) {
+  const { current_point, liquidities } = depthData;
+  const list = Object.values(liquidities);
+  if (list.length > 0) {
+    const priceList: string[] = [];
+    const L_list: string[] = [];
+    const displayList: any[] = [];
+    const decimalRate =
+      Math.pow(10, token_x_decimals) / Math.pow(10, token_y_decimals);
+    let price_c: any = getPriceByPoint(current_point, decimalRate);
+    let price_left: any = getPriceByPoint(left_point, decimalRate);
+    let price_right: any = getPriceByPoint(right_point, decimalRate);
+    if (!sort) {
+      const temp = price_left;
+      price_c = 1 / price_c;
+      price_left = 1 / price_right;
+      price_right = 1 / temp;
+    }
+    list.forEach((item: any, index: number) => {
+      const { left_point, right_point, amount_l } = item;
+      let price_l: any = getPriceByPoint(left_point, decimalRate);
+      let price_r: any = getPriceByPoint(right_point, decimalRate);
+      if (!sort) {
+        const temp = price_l;
+        price_l = 1 / price_r;
+        price_r = 1 / temp;
+      }
+      const min_decimals = _.min([token_x_decimals, token_y_decimals]);
+      const l = toReadableNumber(min_decimals, amount_l);
+      // filter out-of-scope data
+      const maxPrice = new BigNumber(price_c).multipliedBy(1000);
+      const minPrice = new BigNumber(price_c).dividedBy(1000);
+      const needFilterData =
+        minPrice.isGreaterThanOrEqualTo(price_l) ||
+        minPrice.isGreaterThanOrEqualTo(price_r) ||
+        maxPrice.isLessThanOrEqualTo(price_l) ||
+        maxPrice.isLessThanOrEqualTo(price_r);
+      if (!needFilterData) {
+        priceList.push(price_l, price_r);
+        L_list.push(l);
+        displayList.push({
+          left: price_l,
+          right: price_r,
+          l,
+        });
+      }
+    });
+    priceList.push(price_c);
+    priceList.push(price_left);
+    priceList.push(price_right);
+    priceList.sort((a: string, b: string) => {
+      return new BigNumber(a).minus(new BigNumber(b)).toNumber();
+    });
+    L_list.sort((a: string, b: string) => {
+      return new BigNumber(a).minus(new BigNumber(b)).toNumber();
+    });
+    const left_p: any = priceList[0];
+    const right_p: any = priceList[priceList.length - 1];
+    const l_r: any = L_list[L_list.length - 1];
+    const width = chartDom.current.clientWidth - 30;
+    const l: any = new BigNumber(left_p).multipliedBy(0.95).toFixed();
+    const r: any = new BigNumber(right_p).multipliedBy(1.05).toFixed();
+    const myScaleX = d3.scaleLinear().domain([l, r]).range([0, width]);
+    const myScaleY = d3.scaleLinear().domain([0, l_r]).range([0, 200]);
+    const axis: any = d3.axisBottom(myScaleX);
+    axis.ticks(5).tickFormat(function (d: any) {
+      const dBig = new BigNumber(d);
+      if (dBig.isGreaterThanOrEqualTo(10000)) {
+        return dBig.toExponential(1);
+      } else {
+        return d;
+      }
+    });
+    axis.tickSize(0).tickPadding(15);
+    d3.select('.g').call(axis).selectAll('text').style('fill', '#7E8A93');
+    // chart
+    d3.select('.chart')
+      .selectAll('rect')
+      .data(displayList)
+      .join('rect')
+      .attr('width', function (d, i) {
+        const { left, right, l } = d;
+        return myScaleX(right) - myScaleX(left);
+      })
+      .attr('height', function (d, i) {
+        const { left, right, l } = d;
+        return myScaleY(l);
+      })
+      .attr('x', function (d, i) {
+        const { left, right, l } = d;
+        return myScaleX(left) + 15;
+      })
+      .attr('y', function (d, i) {
+        const { left, right, l } = d;
+        return 200 - myScaleY(l);
+      })
+      .style('fill', 'rgba(91, 64, 255, 0.5)');
+    d3.select('.g2')
+      .selectAll('line')
+      .data([price_c])
+      .join('line')
+      .attr('x1', function (d) {
+        return myScaleX(d) + 15;
+      })
+      .attr('x2', function (d) {
+        return myScaleX(d) + 15;
+      })
+      .attr('y1', 0)
+      .attr('y2', 200)
+      .style('strokeWidth', 1)
+      .style('stroke', '#fff');
+
+    d3.select('.gLeftLine')
+      .selectAll('line')
+      .data([price_left])
+      .join('line')
+      .attr('x1', function (d) {
+        return myScaleX(d) + 15;
+      })
+      .attr('x2', function (d) {
+        return myScaleX(d) + 15;
+      })
+      .attr('y1', 0)
+      .attr('y2', 200)
+      .style('strokeWidth', 1)
+      .style('stroke', '#00FFD1');
+
+    d3.select('.gLeftLine')
+      .selectAll('text')
+      .data([price_left])
+      .join('text')
+      .attr('class', 'textLeft')
+      .text(function (d, i) {
+        const diff: any = new BigNumber(price_c).minus(d).abs();
+        const percent = diff.dividedBy(price_c).multipliedBy(100);
+        let p: any;
+        if (percent.isGreaterThanOrEqualTo(10)) {
+          p = percent.toFixed(0);
+        } else {
+          p = percent.toFixed(1);
+        }
+        if (percent.isEqualTo(0)) {
+          return '0%';
+        } else if (price_c > d) {
+          if (percent.isLessThanOrEqualTo(0.1)) {
+            return '>-0.1%';
+          } else {
+            return `-${p}%`;
+          }
+        } else {
+          if (percent.isLessThanOrEqualTo(0.1)) {
+            return '<0.1%';
+          } else {
+            return `${p}%`;
+          }
+        }
+      })
+      .style('fill', '#00FFD1')
+      .style('font-size', '14px')
+      .attr('x', function (d, i) {
+        const textWidth = d3.select('.textLeft').node().getComputedTextLength();
+        const normalPosition = myScaleX(d) + 15 - textWidth - 5;
+        const oppositePosition = myScaleX(d) + 15 + 5;
+        if (normalPosition <= 0) {
+          return oppositePosition;
+        } else {
+          return normalPosition;
+        }
+      })
+      .attr('y', function (d, i) {
+        return 15;
+      });
+
+    d3.select('.gRightLine')
+      .selectAll('line')
+      .data([price_right])
+      .join('line')
+      .attr('x1', function (d) {
+        return myScaleX(d) + 15;
+      })
+      .attr('x2', function (d) {
+        return myScaleX(d) + 15;
+      })
+      .attr('y1', 0)
+      .attr('y2', 200)
+      .style('strokeWidth', 1)
+      .style('stroke', '#00FFD1');
+
+    d3.select('.gRightLine')
+      .selectAll('text')
+      .data([price_right])
+      .join('text')
+      .attr('class', 'textRight')
+      .attr('y', function (d, i) {
+        return 15;
+      })
+      .text(function (d, i) {
+        let result;
+        const diff = new BigNumber(price_c).minus(d).abs();
+        let p = diff.dividedBy(price_c).multipliedBy(100);
+        if (new BigNumber(10000).isLessThanOrEqualTo(p)) {
+          result = p.toExponential(1);
+        } else {
+          if (p.isGreaterThanOrEqualTo(10)) {
+            result = p.toFixed(0);
+          } else {
+            result = p.toFixed(1);
+          }
+        }
+        if (p.isEqualTo(0)) {
+          return '0%';
+        } else if (price_c > d) {
+          if (p.isLessThanOrEqualTo(0.1)) {
+            return '>-0.1%';
+          } else {
+            return `-${result}%`;
+          }
+        } else {
+          if (p.isLessThanOrEqualTo(0.1)) {
+            return '<0.1%';
+          } else {
+            return `${result}%`;
+          }
+        }
+      })
+      .style('fill', '#00FFD1')
+      .style('font-size', '14px')
+      .attr('x', function (d, i) {
+        const textWidth = d3
+          .select('.textRight')
+          .node()
+          .getComputedTextLength();
+        const maxPosition = myScaleX(d) + 15 + textWidth + 5;
+        const oppositePosition = myScaleX(d) + 15 - textWidth - 5;
+        const normalposition = myScaleX(d) + 15 + 5;
+        if (maxPosition >= width) {
+          return oppositePosition;
+        } else {
+          return normalposition;
+        }
+      });
+  }
+  return list.length;
 }
