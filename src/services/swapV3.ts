45daea357786aa3ec04bbd1b4a04ca5034582506
Max <max@ref.finance>
2022-12-03 12:06:28 +0800

    get cached dcl pool

--- src/services/swapV3.ts
+++ src/services/swapV3.ts
@@ -530,6 +530,16 @@ export const get_pool = async (pool_id: string, token0?: string) => {
 
   const new_pool_id = `${token_seq}|${fee}`;
 
+  const cachedPools = localStorage.getItem(REF_DCL_POOL_CACHE_KEY);
+
+  const foundPool =
+    cachedPools &&
+    JSON.parse(cachedPools).find((pool: any) => pool.pool_id === new_pool_id);
+
+  if (foundPool) {
+    return foundPool;
+  }
+
   return refSwapV3ViewFunction({
     methodName: 'get_pool',
     args: {
