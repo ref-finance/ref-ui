4e0bbd28df39b21489d6c67f73ac725cccdf34eb
Max <max@ref.finance>
2022-12-18 00:05:36 +0800

    add activity banner

---
+++ src/context/XmasActivity.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+function XmaxPopUP({
+  xmasModalOpen,
+  setXmasModalOpen,
+}: {
+  xmasModalOpen: boolean;
+  setXmasModalOpen: (xmasModalOpen: boolean) => void;
+}) {
+  return <div></div>;
+}
+
+interface XmasActivityContextValue {
+  xmasModalOpen: boolean;
+  setXmasModalOpen: (xmasModalOpen: boolean) => void;
+}
+
+const XmasActivityContext =
+  React.createContext<XmasActivityContextValue | null>(null);
+
+export const XmasActivityContextProvider: React.FC = ({ children }) => {
+  const [xmasModalOpen, setXmasModalOpen] = useState(false);
+
+  return (
+    <XmasActivityContext.Provider
+      value={{
+        xmasModalOpen,
+        setXmasModalOpen,
+      }}
+    >
+      {children}
+      <XmaxPopUP
+        xmasModalOpen={xmasModalOpen}
+        setXmasModalOpen={setXmasModalOpen}
+      />
+    </XmasActivityContext.Provider>
+  );
+};
+
+export function useXmasActivity() {
+  const context = React.useContext(XmasActivityContext);
+  if (context === undefined) {
+    throw new Error(
+      'useXmasActivity must be used within a XmasActivityContextProvider'
+    );
+  }
+  return context;
+}
