8ab3ec81ef52445339bed237fde0f6303ffcb27c
nature.xie <nature@ref.finance>
2022-12-26 15:31:11 +0800

    filter v2 farm in worker

--- src/worker.ts
+++ src/worker.ts
@@ -184,11 +184,16 @@ async function getXrefPrice(tokenPriceList: Record<string, any>) {
 }
 const cacheBoost_Seed_Farms_Pools = async () => {
   // get all seeds
-  const list_seeds = await get_list_seeds_info();
+  let list_seeds = await get_list_seeds_info();
   // get all farms
   const farmsPromiseList: Promise<any>[] = [];
   const poolIds = new Set<string>();
   let pools: PoolRPCView[] = [];
+  // filter v2 pool seeds TODO
+  list_seeds = list_seeds.filter((seed: Seed) => {
+    if (seed.seed_id.indexOf(config.REF_UNI_V3_SWAP_CONTRACT_ID) == -1)
+      return true;
+  });
   list_seeds.forEach((seed: Seed) => {
     const { seed_id } = seed;
     if (seed_id.indexOf('@') > -1) {
