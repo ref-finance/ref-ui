d68055b7a1c4375f04d193b4ea797b3556af3737
Max <max@ref.finance>
2023-03-03 12:21:42 +0800

    fix order list style

--- src/pages/Orderly/components/AllOrders/index.tsx
+++ src/pages/Orderly/components/AllOrders/index.tsx
@@ -438,7 +438,7 @@ function OrderLine({
       <td
         className={`col-span-1 py-3 relative  ${
           openEdit ? 'items-start ' : 'items-center'
-        } ${showCurSymbol ? 'pl-5' : 'pl-0'} `}
+        } ${showCurSymbol ? 'pl-5' : 'pl-0 transform translate-x-1/3'} `}
       >
         <TextWrapper
           className="px-2 text-sm"
@@ -775,7 +775,7 @@ function HistoryOrderLine({
           {marketInfo}
         </td>
 
-        <td className={`col-span-1 py-3 ${showCurSymbol ? 'pl-5' : ''}`}>
+        <td className={`col-span-1 py-3 ${showCurSymbol ? 'pl-5' : 'transform translate-x-1/3'}`}>
           <TextWrapper
             className="px-2 text-sm"
             value={order.side === 'BUY' ? 'Buy' : 'Sell'}
@@ -1213,7 +1213,7 @@ function OpenOrders({
             </FlexRow>
           </th>
 
-          <th>
+          <th className={showCurSymbol ? '' :'transform translate-x-1/3'}>
             <FlexRow
               className={`col-span-1 py-2 ${
                 showCurSymbol ? 'pl-5' : ''
@@ -1563,7 +1563,7 @@ function HistoryOrders({
           <img
             src={fromToken?.icon}
             alt=""
-            className="rounded-full w-5 h-5 mr-2"
+            className="rounded-full flex-shrink-0 w-5 h-5 mr-2"
           />
 
           <span>{symbolFrom}</span>
@@ -1642,7 +1642,7 @@ function HistoryOrders({
             )}
           </th>
 
-          <th className="py-2.5">
+          <th className={`py-2.5 ${showCurSymbol ? '' :'transform translate-x-1/3'}`}>
             <FlexRow
               className={`col-span-1  relative ${showCurSymbol ? 'pl-5' : ''}`}
             >
--- src/pages/Orderly/orderly/off-chain-ws.ts
+++ src/pages/Orderly/orderly/off-chain-ws.ts
@@ -39,9 +39,14 @@ export const useOrderlyWS = () => {
     useWebSocket(socketUrl, {
       shouldReconnect: (closeEvent) => true,
       reconnectAttempts: 10,
-      //attemptNumber will be 0 the first time it attempts to reconnect, so this equation results in a reconnect pattern of 1 second, 2 seconds, 4 seconds, 8 seconds, and then caps at 10 seconds until the maximum number of attempts is reached
       reconnectInterval: (attemptNumber) =>
         Math.min(Math.pow(2, attemptNumber) * 1000, 10000),
+      onClose: (e) => {
+        alert(e.type);
+      },
+      onError: (e) => {
+        alert(e.type);
+      },
     });
 
   useEffect(() => {
