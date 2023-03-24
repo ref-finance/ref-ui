9066d9e5bfdf994e7fc6813550f1bb4f587c2b68
nature.xie <nature@ref.finance>
2023-03-24 08:59:48 +0800

    fix unable click button issue

--- src/components/forms/SelectToken.tsx
+++ src/components/forms/SelectToken.tsx
@@ -733,7 +733,7 @@ export default function SelectToken({
             </localTokens.Provider>
           </div>
           {searchNoData ? (
-            <div className="flex flex-col  items-center justify-center mt-12">
+            <div className="flex flex-col  items-center justify-center mt-12 relative z-10">
               <div className="text-sm text-farmText">
                 <FormattedMessage id="no_token_found"></FormattedMessage>
               </div>
