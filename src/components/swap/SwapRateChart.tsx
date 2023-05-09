0df3418a46878546e34b6b09e532425eadafb090
Max <max@ref.finance>
2023-05-09 17:29:24 +0800

    fix tick ui

--- src/components/swap/SwapRateChart.tsx
+++ src/components/swap/SwapRateChart.tsx
@@ -440,12 +440,13 @@ export default function SwapRateChart(props: SwapRateChartProps) {
         </div>
 
         {diff && (
-          <div className=" lg:hidden text-primaryText ml-4 flex flex-col items-end text-10px ">
-            <FormattedMessage
-              id="last_updated"
-              defaultMessage={'Last Updated'}
-            ></FormattedMessage>
-            <br />
+          <div className=" lg:hidden text-primaryText ml-4 flex flex-col items-end text-10px text-right">
+            <span className="whitespace-nowrap">
+              <FormattedMessage
+                id="last_updated"
+                defaultMessage={'Last Updated'}
+              ></FormattedMessage>
+            </span>
 
             <span className="ml-1">{diff.lastUpdate}</span>
           </div>
@@ -491,9 +492,14 @@ export default function SwapRateChart(props: SwapRateChartProps) {
       </div>
 
       {diff && (
-        <div className="frcs">
-          <div className="lg:hidden text-primaryText frcs  text-sm">
-            <span>
+        <div
+          className="frcs  "
+          style={{
+            overflow: 'anywhere',
+          }}
+        >
+          <div className="lg:hidden flex-wrap text-primaryText frcs   text-sm">
+            <span className="mr-1.5">
               <FormattedMessage
                 id="low"
                 defaultMessage={'Low'}
@@ -501,7 +507,7 @@ export default function SwapRateChart(props: SwapRateChartProps) {
               {`(${displayDimension})`}
             </span>
 
-            <span className="font-gothamBold ml-1.5 text-white">
+            <span className="font-gothamBold  text-white">
               {priceList &&
                 priceFormatter(
                   minBy(priceList.price_list, (p) => p.price)?.price || 0
@@ -509,8 +515,8 @@ export default function SwapRateChart(props: SwapRateChartProps) {
             </span>
           </div>
 
-          <div className="frcs lg:hidden ml-7 text-primaryText text-sm">
-            <span>
+          <div className="frcs flex-wrap lg:hidden  ml-7 text-primaryText text-sm">
+            <span className="mr-1.5">
               <FormattedMessage
                 id="high"
                 defaultMessage={'High'}
@@ -518,7 +524,7 @@ export default function SwapRateChart(props: SwapRateChartProps) {
               {`(${displayDimension})`}
             </span>
 
-            <span className="font-gothamBold ml-1.5 text-white">
+            <span className="font-gothamBold  text-white">
               {priceList &&
                 priceFormatter(
                   maxBy(priceList.price_list, (p) => p.price)?.price || 0
