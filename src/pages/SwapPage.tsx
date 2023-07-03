33949ff1c214978046c321f3017766fbb8b8863d
Joe Zhu <aidai524@gmail.com>
2023-07-03 17:37:29 +0800

    Change default swap mode (#408)
    * change default swap mode to lite version
    
    * update swap mode
    
    * fix swap mode from limit order to swap
    
    * fix net worth calculation issue
    
    ---------
    
    Co-authored-by: Max <max@ref.finance>

--- src/pages/SwapPage.tsx
+++ src/pages/SwapPage.tsx
@@ -131,6 +131,7 @@ const ChangeSwapMode = ({
           onClick={() => {
             setSwapMode(SWAP_MODE.NORMAL);
             localStorage.setItem(SWAP_MODE_KEY, SWAP_MODE.NORMAL);
+            changeSwapType(SWAP_TYPE.LITE);
           }}
         >
           Swap
@@ -152,7 +153,6 @@ const ChangeSwapMode = ({
             setSwapMode(SWAP_MODE.LIMIT);
             localStorage.setItem(SWAP_MODE_KEY, SWAP_MODE.LIMIT);
             setLimitTokenTrigger(!limitTokenTrigger ? true : false);
-            changeSwapType(SWAP_TYPE.Pro);
           }}
           style={{
             fontSize: '15px',
@@ -215,12 +215,13 @@ function SwapPage() {
     sessionStorage.getItem(SWAP_ENABLE_TRI) === 'true' || false
   );
 
+  const storedType =
+    (sessionStorage.getItem(SWAP_TYPE_KEY) as SWAP_TYPE) || null;
+
   const [selectMarket, setSelectMarket] = useState<SwapMarket>(undefined);
 
-  const storedType = sessionStorage.getItem(SWAP_TYPE_KEY) as SWAP_TYPE | null;
-
   const [swapType, setSwapType] = useState<SWAP_TYPE>(
-    storedType || SWAP_TYPE.Pro
+    storedType || SWAP_TYPE.LITE
   );
 
   const [forceEstimatePro, setForceEstimatePro] = useState<boolean>(false);
@@ -277,6 +278,7 @@ function SwapPage() {
   useEffect(() => {
     if (swapMode === SWAP_MODE.LIMIT) {
       setLimitTokenTrigger(!limitTokenTrigger ? true : false);
+      changeSwapType(SWAP_TYPE.Pro);
     }
   }, [swapMode]);
 
