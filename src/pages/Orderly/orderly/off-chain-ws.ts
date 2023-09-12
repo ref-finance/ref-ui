25b3f8a95955648f1e60390810834f0ec55df1ba
Max <max@ref.finance>
2023-09-12 12:59:39 +0800

    remove ws log

--- src/pages/Orderly/orderly/off-chain-ws.ts
+++ src/pages/Orderly/orderly/off-chain-ws.ts
@@ -85,8 +85,6 @@ export const useOrderlyWS = () => {
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
 
-  console.log('connectionStatus: ', connectionStatus);
-
   const handleVisibilityChange = () => {
     if (document.visibilityState === 'visible') {
       setSocketUrl(orderlySocketUrl);
