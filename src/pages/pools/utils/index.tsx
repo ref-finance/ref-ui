0d933ebaf7cdd22a4a081152d0ea19018f0eda74
Gavin <gavinchen8914@gmail.com>
2023-10-23 18:05:18 +0800

    code fix

--- src/pages/pools/utils/index.tsx
+++ src/pages/pools/utils/index.tsx
@@ -1,5 +1,5 @@
-import { Pool } from "~src/services/pool";
-import { scientificNotationToString, toPrecision } from "~src/utils/numbers";
+import { Pool } from "src/services/pool";
+import { scientificNotationToString, toPrecision } from "src/utils/numbers";
 
 export const REF_FI_POOL_ACTIVE_TAB = 'REF_FI_POOL_ACTIVE_TAB_VALUE';
 
