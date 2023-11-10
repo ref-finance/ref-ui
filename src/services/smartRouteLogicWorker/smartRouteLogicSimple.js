2835fedf853d4d0fb94d92073bf137379759b12a
harry <691742@gmail.com>
2023-11-10 22:52:56 +0800

    fix: swap router format near metadata

--- src/services/smartRouteLogicWorker/smartRouteLogicSimple.js
+++ src/services/smartRouteLogicWorker/smartRouteLogicSimple.js
@@ -3,7 +3,11 @@
 ////////////////////////////////////////////////////////////////////////////
 import Big from 'big.js';
 
+import * as CONSTANTS from '../../constants';
 import db from '../../store/RefDatabase';
+import metadataDefaults from '../../utils/metadata';
+
+const { NEAR_META_DATA, WRAP_NEAR_CONTRACT_ID } = CONSTANTS;
 
 Big.RM = 0;
 Big.DP = 40;
@@ -1122,6 +1126,14 @@ async function getSmartRouteSwapActions(
   return actions;
 }
 
+const NEAR_ICON =
+  'https://near.org/wp-content/themes/near-19/assets/img/brand-icon.png';
+const BANANA_ID = 'berryclub.ek.near';
+const CHEDDAR_ID = 'token.cheddar.near';
+const CUCUMBER_ID = 'farm.berryclub.ek.near';
+const HAPI_ID = 'd9c2d319cd7e6177336b0a9c93c21cb48d84fb54.factory.bridge.near';
+const WOO_ID = '4691937a7508860f876c9c0a2a617e7d9e945d4b.factory.bridge.near';
+
 const ftGetTokenMetadata = async (id) => {
   try {
     const metadata = await db.allTokens().where({ id }).first();
@@ -1138,24 +1150,24 @@ const ftGetTokenMetadata = async (id) => {
     //   });
     // }
 
-    // if (metadata.id === WRAP_NEAR_CONTRACT_ID) {
-    //   return {
-    //     ...metadata,
-    //     icon: nearMetadata.icon,
-    //     symbol: 'NEAR',
-    //   };
-    // } else if (
-    //   !metadata.icon ||
-    //   metadata.icon === NEAR_ICON ||
-    //   metadata.id === BANANA_ID ||
-    //   metadata.id === CHEDDAR_ID ||
-    //   metadata.id === CUCUMBER_ID ||
-    //   metadata.id === HAPI_ID ||
-    //   metadata.id === WOO_ID ||
-    //   metadata.id === WRAP_NEAR_CONTRACT_ID
-    // ) {
-    //   metadata.icon = metadataDefaults[id];
-    // }
+    if (metadata.id === WRAP_NEAR_CONTRACT_ID) {
+      return {
+        ...metadata,
+        icon: NEAR_META_DATA.icon,
+        symbol: NEAR_META_DATA.symbol,
+      };
+    } else if (
+      !metadata.icon ||
+      metadata.icon === NEAR_ICON ||
+      metadata.id === BANANA_ID ||
+      metadata.id === CHEDDAR_ID ||
+      metadata.id === CUCUMBER_ID ||
+      metadata.id === HAPI_ID ||
+      metadata.id === WOO_ID ||
+      metadata.id === WRAP_NEAR_CONTRACT_ID
+    ) {
+      metadata.icon = metadataDefaults[id];
+    }
     return {
       id,
       ...metadata,
