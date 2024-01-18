3492236eed384a6d9ecd58bc29cefa97668e9d8a
nature.xie <nature@ref.finance>
2024-01-18 16:56:34 +0800

    handle callback ui

--- src/components/meme/CallBackModal.tsx
+++ src/components/meme/CallBackModal.tsx
@@ -1,19 +1,23 @@
 import React, { useContext, useMemo } from 'react';
+import Big from 'big.js';
 import { OprationButton } from '../../components/button/Button';
 import { isMobile } from '../../utils/device';
-import { ModalCloseIcon } from './icons';
+import { ModalCloseIcon, ArrowRightIcon } from './icons';
 import Modal from 'react-modal';
 import { MemeContext } from './context';
 import { ITxParams } from './SeedsBox';
 import { Seed } from '../../services/farm';
 import { toReadableNumber } from '../../utils/numbers';
-import { formatWithCommas_number } from '../../utils/uiNumber';
+import {
+  formatWithCommas_number,
+  formatPercentage,
+} from '../../utils/uiNumber';
 import { openUrl } from '../../services/commonV3';
 import getConfig from '../../services/config';
 import { useHistory } from 'react-router';
 
 function CallBackModal(props: any) {
-  const { seeds } = useContext(MemeContext);
+  const { seeds, tokenPriceList } = useContext(MemeContext);
   const {
     isOpen,
     onRequestClose,
@@ -23,22 +27,49 @@ function CallBackModal(props: any) {
   const cardWidth = isMobile() ? '90vw' : '28vw';
   const cardHeight = isMobile() ? '90vh' : '80vh';
   let b: ITxParams;
-  const seed: Seed = useMemo(() => {
-    const { action, receiver_id, params, txHash } = txParams;
+  const [seed, amount] = useMemo(() => {
+    const { action, receiver_id, params } = txParams;
     if (action == 'stake') {
-      return seeds[receiver_id];
+      const seed = seeds[receiver_id];
+      return [
+        seed,
+        toReadableNumber(seed?.seed_decimal || 0, txParams.params.amount),
+      ];
     } else {
-      return seeds[params.seed_id];
+      const seed = seeds[params.seed_id];
+      return [
+        seed,
+        toReadableNumber(
+          seed?.seed_decimal || 0,
+          txParams.params.unstake_amount
+        ),
+      ];
     }
   }, [seeds, txParams]);
+  const [weightFrom, weightTo] = useMemo(() => {
+    const { action } = txParams;
+    const totalTvl = Object.entries(seeds).reduce((acc, [, seed]) => {
+      return acc.plus(seed?.seedTvl || 0);
+    }, Big(0));
+    const seedTvl = seed?.seedTvl || 0;
+    const actionTvl = Big(amount || 0).mul(
+      tokenPriceList[seed?.seed_id]?.price || 0
+    );
+    let from;
+    const to = totalTvl.gt(0) ? Big(seedTvl).div(totalTvl).mul(100) : Big(0);
+    if (action == 'stake') {
+      from = totalTvl.minus(actionTvl).gt(0)
+        ? Big(seedTvl).minus(actionTvl).div(totalTvl.minus(actionTvl)).mul(100)
+        : Big(0);
+    } else {
+      from = totalTvl.plus(actionTvl).gt(0)
+        ? Big(seedTvl).plus(actionTvl).div(totalTvl.plus(actionTvl)).mul(100)
+        : Big(0);
+    }
+    return [from.toFixed(), to.toFixed()];
+  }, [amount, seeds, txParams, seed]);
+
   if (!seed) return null;
-  function getAmount() {
-    const amount =
-      txParams.action == 'stake'
-        ? txParams.params.amount
-        : txParams.params.unstake_amount;
-    return formatWithCommas_number(toReadableNumber(seed.seed_decimal, amount));
-  }
   function goNearblocks() {
     history.replace('/meme');
     onRequestClose();
@@ -93,12 +124,22 @@ function CallBackModal(props: any) {
             </div>
             <div className="flex justify-center text-sm text-white mt-6">
               You have just {txParams.action == 'stake' ? 'feed' : 'unstake'}{' '}
-              {getAmount()} {seed?.token_meta_data.symbol}{' '}
+              {formatWithCommas_number(amount)} {seed?.token_meta_data.symbol}{' '}
+            </div>
+            <div className="flex justify-center text-sm text-white mt-3">
+              Gauge Weight
+              <div className="flex items-center text-sm text-white gap-2 ml-3">
+                <span className="line-through">
+                  {formatPercentage(weightFrom)}
+                </span>
+                <ArrowRightIcon />
+                <span>{formatPercentage(weightTo)}</span>
+              </div>
             </div>
             <OprationButton
               minWidth="7rem"
               onClick={goNearblocks}
-              className={`flex flex-grow items-center justify-center border border-purple border-opacity-20  text-white mt-6 rounded-xl h-12 text-base gotham_bold focus:outline-none`}
+              className={`flex flex-grow items-center justify-center border border-purple border-opacity-20  text-white mt-12 rounded-xl h-12 text-base gotham_bold focus:outline-none`}
             >
               Check on Nearblocks
             </OprationButton>
