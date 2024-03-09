5b4e44ba1179936406a4aa8994bea8d6c30ec117
xieqian <dss@192.168.0.101>
2024-03-09 22:30:06 +0800

    disbaled tris for mintbase  wallet

--- src/components/forms/SlippageSelector.tsx
+++ src/components/forms/SlippageSelector.tsx
@@ -217,7 +217,8 @@ export default function SlippageSelector({
   const selectedWalletId = window.selector?.store?.getState()?.selectedWalletId;
   const trisDisbaled =
     selectedWalletId === 'near-snap' ||
-    selectedWalletId === 'near-mobile-wallet';
+    selectedWalletId === 'near-mobile-wallet' ||
+    selectedWalletId === 'mintbase-wallet';
   useEffect(() => {
     if (trisDisbaled) {
       setEnableTri(false);
