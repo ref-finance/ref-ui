34016a5c40fc42126704c6af1903e603aa26be9b
deep-path <476044723@qq.com>
2024-04-23 17:02:32 +0800

    fix
    change file name

--- src/components/icon/memeComingModal/index.tsx
+++ src/components/icon/memeComingModal/index.tsx
@@ -3,7 +3,7 @@ import { RuleTips } from './ruleTips';
 import { TriangleUp } from './triangleUp';
 import { TriangleDown } from './triangleDown';
 import Uwon from './uwon';
-import ShillGpt from './shillGpt';
+import ShillGpt from './nShillGpt';
 import UwonGreen from './uwonGreen';
 
 export { Coin, RuleTips, TriangleUp, TriangleDown, Uwon, ShillGpt, UwonGreen };
