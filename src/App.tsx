1f8870049288a9c1f0091a5e11bdbd1a1dcaf0e6
xieqiancaosissi <nature@ref.finance>
2024-05-11 12:15:56 +0800

    add log (#527)
    Co-authored-by: xieqian <dss@192.168.0.100>

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { XmasActivityContextProvider } from './context/XmasActivity';
 import { ModalGAPrivacy } from 'src/context/modal-ui/modalGAPrivacy/modalGAPrivacy';
 import RpcList from 'src/components/rpc';
 function App() {
+  console.log('88888888', process.env.REACT_APP_NEAR_ENV);
   return (
     <Router>
       <WalletSelectorContextProvider>
