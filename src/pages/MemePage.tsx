53047505c6a075e9616ec434e7ee1caa9759e94e
lq0-github <1441665200@qq.com>
2024-05-23 16:21:45 +0800

    Add Countdown ends

--- src/pages/MemePage.tsx
+++ src/pages/MemePage.tsx
@@ -11,13 +11,16 @@ import Countdown from '../components/meme/countdown';
 
 export default function MemePage() {
   const is_mobile = isMobile();
+  const now = new Date();
+  const targetDate = new Date(now.getFullYear(), 5, 5, 0, 0, 0);
+  const shouldShowCountdown = now < targetDate;
   return (
     <MemeContextProvider>
       <div className="-mt-12 xsm:mt-0">
         {is_mobile ? <MobileBanner /> : <Banner />}
         <div className="m-auto lg:w-5/6" style={{ maxWidth: '1100px' }}>
           <Overview />
-          <Countdown />
+          {shouldShowCountdown && <Countdown />}
           <VoteXREF />
           <SeedsBox />
           <WithdrawList />
