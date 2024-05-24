582659101920fe19cfd07ee379028e5a6cd25617
xieqiancaosissi <nature@ref.finance>
2024-05-24 21:59:47 +0800

    Add meme countdown (#534)
    * update txHash pikespeak
    
    * update-orderly-tip
    
    * Add meme countdown
    
    * Update meme countdown to Feed
    
    * update woo icon address
    
    * Add Countdown ends
    
    * Add Limit Order tx
    
    * update UTC meme time
    
    * Fix some bugs
    
    ---------
    
    Co-authored-by: lq0-github <1441665200@qq.com>
    Co-authored-by: xieqian <dss@192.168.0.101>
    Co-authored-by: xieqian <dss@dsss-MacBook-Pro.local>

--- src/pages/MemePage.tsx
+++ src/pages/MemePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Banner from '../components/meme/Banner';
 import { isMobile } from '../utils/device';
 import { MemeContextProvider } from '../components/meme/context';
@@ -7,15 +7,23 @@ import Overview from '../components/meme/Overview';
 import VoteXREF from '../components/meme/VoteXREF';
 import SeedsBox from '../components/meme/SeedsBox';
 import WithdrawList from '../components/meme/WithdrawList';
+import Countdown from '../components/meme/countdown';
 
 export default function MemePage() {
   const is_mobile = isMobile();
+  const [showCountdown, setShowCountdown] = useState(true);
+  const handleCountdownFinish = () => {
+    setShowCountdown(false);
+  };
   return (
     <MemeContextProvider>
       <div className="-mt-12 xsm:mt-0">
         {is_mobile ? <MobileBanner /> : <Banner />}
         <div className="m-auto lg:w-5/6" style={{ maxWidth: '1100px' }}>
           <Overview />
+          {showCountdown && (
+            <Countdown onCountdownFinish={handleCountdownFinish} />
+          )}
           <VoteXREF />
           <SeedsBox />
           <WithdrawList />
