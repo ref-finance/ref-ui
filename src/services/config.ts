31bdf8c039ca0e66e369f46fc427f161a0d5dd61
李茜 <1441665200@qq.com>
2024-06-05 14:57:52 +0800

    update meme api

--- src/services/config.ts
+++ src/services/config.ts
@@ -151,7 +151,7 @@ export default function getConfig(
         indexerUrl: 'https://api.ref.finance',
         sodakiApiUrl: 'https://api.stats.ref.finance/api',
         txIdApiUrl: 'https://api3.nearblocks.io',
-        memeRankApiUrl: 'https://test.api.cclp.finance',
+        memeRankApiUrl: 'https://test.api.cclp.finance/v3',
         blackList: process.env.FARM_BLACK_LIST || ['1371#3', '2769#2'],
         REF_FI_CONTRACT_ID:
           process.env.REF_FI_CONTRACT_ID || 'v2.ref-finance.near',
@@ -321,7 +321,7 @@ export default function getConfig(
         indexerUrl: 'https://testnet-indexer.ref-finance.com',
         sodakiApiUrl: 'https://api.stats.ref.finance/api',
         txIdApiUrl: 'https://api-testnet.nearblocks.io',
-        memeRankApiUrl: 'https://test.api.cclp.finance',
+        memeRankApiUrl: 'https://test.api.cclp.finance/v3',
         blackList: process.env.FARM_BLACK_LIST || ['1371#3'],
         REF_FI_CONTRACT_ID:
           process.env.REF_FI_CONTRACT_ID || 'ref-finance-101.testnet',
@@ -421,7 +421,7 @@ export default function getConfig(
         indexerUrl: 'https://dev-indexer.ref-finance.com',
         sodakiApiUrl: 'https://api.stats.ref.finance/api',
         txIdApiUrl: 'https://api-testnet.nearblocks.io',
-        memeRankApiUrl: 'https://test.api.cclp.finance',
+        memeRankApiUrl: 'https://test.api.cclp.finance/v3',
         blackList: process.env.FARM_BLACK_LIST || ['1371#3'],
         REF_FI_CONTRACT_ID:
           process.env.REF_FI_CONTRACT_ID || 'exchange.ref-dev.testnet',
@@ -521,7 +521,7 @@ export default function getConfig(
         indexerUrl: 'https://api.ref.finance',
         sodakiApiUrl: 'https://api.stats.ref.finance/api',
         txIdApiUrl: 'https://api3.nearblocks.io',
-        memeRankApiUrl: 'https://test.api.cclp.finance',
+        memeRankApiUrl: 'https://test.api.cclp.finance/v3',
         blackList: process.env.FARM_BLACK_LIST || ['1371#3', '2769#2'],
         REF_FI_CONTRACT_ID:
           process.env.REF_FI_CONTRACT_ID || 'v2.ref-finance.near',
