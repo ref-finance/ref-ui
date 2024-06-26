38806ed1a11f719db72f9a13560388ea2b3be4b5
deep-path <476044723@qq.com>
2024-06-26 16:21:31 +0800

    feat: classic pool use test interface

--- src/pages/pools/LiquidityPage/LiquidityPage.tsx
+++ src/pages/pools/LiquidityPage/LiquidityPage.tsx
@@ -317,6 +317,7 @@ function PoolRow({
                 </div>
               )}
               {supportFarm && <FarmStampNew multi={farmCount > 1} />}
+              {pool.top && (
                 <div
                   className="w-9 h-5 bg-greenLight text-black font-bold text-xs frcc ml-2"
                   style={{ borderRadius: ' 14px' }}
