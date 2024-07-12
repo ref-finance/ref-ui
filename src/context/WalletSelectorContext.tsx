10b7329a1a115baf063c33c44859293fa512d2da
xieqiancaosissi <nature@ref.finance>
2024-07-12 14:34:22 +0800

    Add pagoda rpc (#564)
    * add rpc
    
    * add custom rpc for walletSelector
    
    * remove pagodaRpc

--- src/context/WalletSelectorContext.tsx
+++ src/context/WalletSelectorContext.tsx
@@ -2,7 +2,11 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { map, distinctUntilChanged } from 'rxjs';
 
 import { NetworkId, setupWalletSelector } from '@near-wallet-selector/core';
-import type { WalletSelector, AccountState } from '@near-wallet-selector/core';
+import type {
+  WalletSelector,
+  AccountState,
+  Network,
+} from '@near-wallet-selector/core';
 import { setupModal } from '@near-wallet-selector/modal-ui';
 import type { WalletSelectorModal } from '@near-wallet-selector/modal-ui';
 import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
@@ -16,7 +20,10 @@ import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
 
 import { setupNightly } from '@near-wallet-selector/nightly';
 
-import getConfig from '../services/config';
+import getConfig, {
+  getExtendConfig,
+  getCustomConfig,
+} from '../services/config';
 import { setupWalletConnect } from '@near-wallet-selector/wallet-connect';
 import { setupNearMobileWallet } from '@near-wallet-selector/near-mobile-wallet';
 import { setupOKXWallet } from '@near-wallet-selector/okx-wallet';
@@ -131,8 +138,22 @@ export const WalletSelectorContextProvider: React.FC<any> = ({ children }) => {
   };
 
   const init = useCallback(async () => {
+    const RPC_LIST_system = getExtendConfig().RPC_LIST;
+    const RPC_LIST_custom = getCustomConfig();
+    const RPC_LIST = Object.assign(RPC_LIST_system, RPC_LIST_custom) as any;
+    let endPoint = 'defaultRpc';
+    try {
+      endPoint = window.localStorage.getItem('endPoint') || endPoint;
+      if (!RPC_LIST[endPoint]) {
+        endPoint = 'defaultRpc';
+        localStorage.removeItem('endPoint');
+      }
+    } catch (error) {}
     const _selector = await setupWalletSelector({
-      network: getConfig().networkId as NetworkId,
+      network: {
+        networkId: getConfig().networkId as NetworkId,
+        nodeUrl: RPC_LIST[endPoint].url,
+      } as Network,
       debug: false,
       modules: [
         setupOKXWallet({}),
