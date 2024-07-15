0ca9bd40e5b29f828fe44111c77dbf22d572684f
nature <nature@ref.finance>
2024-07-15 14:02:48 +0800

    remove ledger tip

--- src/context/WalletSelectorContext.tsx
+++ src/context/WalletSelectorContext.tsx
@@ -278,14 +278,12 @@ export const WalletSelectorContextProvider: React.FC<any> = ({ children }) => {
 
     const allKeys = (await account.getAccessKeys()) as IAccountKey[];
 
-    const inUsedKey = selector.store.getState().accounts?.[0]?.publicKey || '';
     const isWalletMeta = allKeys.some((k) => {
       if (k.access_key.permission === 'FullAccess') return false;
       const meta = (
         k.access_key.permission as Ipermission
       ).FunctionCall.method_names.includes('__wallet__metadata');
-      const isInUsed = k.public_key == inUsedKey;
-      return meta && isInUsed;
+      return meta;
     });
 
     const isSelectLedger =
