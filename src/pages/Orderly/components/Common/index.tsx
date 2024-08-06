cf14eb954a027d0c9d3e83be3f01b504f2324ffc
xieqiancaosissi <nature@ref.finance>
2024-08-06 14:22:39 +0800

    Revert "replace orderly domain (#563)" (#578)
    This reverts commit 62cb6563cf23600b309d4d92e021e8e451e22525.

--- src/pages/Orderly/components/Common/index.tsx
+++ src/pages/Orderly/components/Common/index.tsx
@@ -326,7 +326,7 @@ export function RegisterButton({
               })}
               <a
                 className="underline text-primary ml-1"
-                href="https://orderly.org/"
+                href="https://orderly.network/"
                 target="_blank"
                 rel="noopener noreferrer nofollow"
               >
