d2773a2f2a4142adfda67be1c099807d56afed9a
harry <691742@gmail.com>
2024-08-15 16:34:52 +0800

    fix: change bridge history api

--- src/pages/Bridge/services/history.ts
+++ src/pages/Bridge/services/history.ts
@@ -15,7 +15,7 @@ const bridgeHistoryService = {
     const { data } = await request<{
       data: { list?: BridgeModel.BridgeHistory[]; total?: number };
     }>(
-      generateUrl(`https://test.api.cclp.finance/v3/bridge/list`, {
+      generateUrl(`https://mainnet-indexer.ref-finance.com/v3/bridge/list`, {
         account_id,
         tx_hash: hash,
         chain: chain === 'NEAR' ? 'aurora' : chain.toLowerCase(),
