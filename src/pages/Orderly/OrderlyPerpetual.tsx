692fce9ae546e4607ef6289cbed850e5fc591df2
nature <nature@ref.finance>
2024-08-23 10:37:45 +0800

    update orderly tip

--- src/pages/Orderly/OrderlyPerpetual.tsx
+++ src/pages/Orderly/OrderlyPerpetual.tsx
@@ -50,7 +50,7 @@ import { parseSymbol } from './components/RecentTrade';
 import { NewUserTip } from './components/Common/NewUserTip';
 import BlockOrderBookTip from './BlockOrderBookTip';
 
-export const REF_ORDERLY_PERP_TIP_SIG = 'REF_ORDERLY_PERP_TIP_SIGN10';
+export const REF_ORDERLY_PERP_TIP_SIG = 'REF_ORDERLY_PERP_TIP_SIGN11';
 
 function TradingBoard() {
   const isLarge = useLargeScreen();
