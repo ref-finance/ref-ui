b3365ab77ec3f1ed79918102a04df23a64be5fdf
nature <nature@ref.finance>
2024-08-30 21:32:59 +0800

    update limit order config

--- src/services/configV2.ts
+++ src/services/configV2.ts
@@ -34,6 +34,8 @@ export default function getConfig(
           'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near|token.v2.ref-finance.near|400',
           'dac17f958d2ee523a2206206994597c13d831ec7.factory.bridge.near|token.v2.ref-finance.near|400',
           '17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1|wrap.near|2000',
+          '853d955acef822db058eb8505911ed77f175b99e.factory.bridge.near|wrap.near|2000',
+          'aurora|wrap.near|2000',
         ],
         SUPPORT_SHADOW_POOL_IDS: ['4179'],
       };
@@ -102,6 +104,8 @@ export default function getConfig(
           'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near|token.v2.ref-finance.near|400',
           'dac17f958d2ee523a2206206994597c13d831ec7.factory.bridge.near|token.v2.ref-finance.near|400',
           '17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1|wrap.near|2000',
+          '853d955acef822db058eb8505911ed77f175b99e.factory.bridge.near|wrap.near|2000',
+          'aurora|wrap.near|2000',
         ],
         SUPPORT_SHADOW_POOL_IDS: ['4179'],
       };
