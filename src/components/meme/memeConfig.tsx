f3fb0ed1ea9aa5fbd97bf119c782b5de33c71d07
naturexie <786281870@qq.com>
2024-10-03 23:42:22 +0800

    feat: add gp.token0.near for check in

--- src/components/meme/memeConfig.tsx
+++ src/components/meme/memeConfig.tsx
@@ -698,6 +698,7 @@ export function getMemeCheckInConfig(): any {
         'token.lonkingnearbackto2024.near',
         'blackdragon.tkn.near',
         'gear.enleap.near',
+        'gp.token0.near',
       ],
       nftMetaData: {
         title: 'MEME Honorary NFT 6',
