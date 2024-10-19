923de7daad73f2f3b8798dcc5643df17196e5355
xieqiancaosissi <nature@ref.finance>
2024-10-19 14:45:21 +0800

    fix: remove no balance meme token (#624)
    * fix: remove no balance meme token
    
    * update config
    
    ---------
    
    Co-authored-by: naturexie <786281870@qq.com>

--- src/components/meme/memeConfig.tsx
+++ src/components/meme/memeConfig.tsx
@@ -699,7 +699,7 @@ export function getMemeCheckInConfig(): any {
         'token.lonkingnearbackto2024.near',
         'blackdragon.tkn.near',
         'gear.enleap.near',
-        'gp.token0.near',
+        // 'gp.token0.near',
         'kat.token0.near',
       ],
       nftMetaData: {
